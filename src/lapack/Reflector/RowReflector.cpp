c8a4c0092936bc384cab399ef5b7446a337543a6
Jack Poulson <jack.poulson@gmail.com>
2010-08-29 21:13:45 -0500

    Migrated back to New BSD License and added 'pragma once' directives at the top of all include files.

--- src/lapack/Reflector/RowReflector.cpp
+++ src/lapack/Reflector/RowReflector.cpp
@@ -1,25 +1,38 @@
 /*
-   Copyright (C) 1992-2008 The University of Tennessee. 
+   Copyright (c) 1992-2008 The University of Tennessee. 
    All rights reserved.
 
-   Copyright (C) 2009-2010 Jack Poulson <jack.poulson@gmail.com>
+   Copyright (c) 2009-2010, Jack Poulson
+   All rights reserved.
 
-   This file is part of Elemental.
+   This file is part of Elemental and is partially based upon the LAPACK 
+   routines dlarfg.f and zlarfg.f.
 
-   Elemental is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
 
-   Elemental is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    - Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
 
-   You should have received a copy of the GNU General Public License
-   along with Elemental.  If not, see <http://www.gnu.org/licenses/>.
+    - Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
 
-   This file is partially based upon the LAPACK routines dlarfg.f and zlarfg.f.
+    - Neither the name of the owner nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+   AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+   LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
 */
 #include "elemental/lapack_internal.hpp"
 using namespace std;
