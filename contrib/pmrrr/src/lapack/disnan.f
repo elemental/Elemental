ca719b81a50c617035bea29f646475c7ca3c9bf5
Jack Poulson <jack.poulson@gmail.com>
2010-12-01 09:32:12 -0600

    Addings LAPACK source back into PMRRR in case the machine does not have a recent enough LAPACK library for PMRRR.

---
+++ contrib/pmrrr/src/lapack/disnan.f
@@ -0,0 +1,34 @@
+      LOGICAL FUNCTION DISNAN( DIN )
+*
+*  -- LAPACK auxiliary routine (version 3.2.2) --
+*  -- LAPACK is a software package provided by Univ. of Tennessee,    --
+*  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
+*     June 2010
+*
+*     .. Scalar Arguments ..
+      DOUBLE PRECISION   DIN
+*     ..
+*
+*  Purpose
+*  =======
+*
+*  DISNAN returns .TRUE. if its argument is NaN, and .FALSE.
+*  otherwise.  To be replaced by the Fortran 2003 intrinsic in the
+*  future.
+*
+*  Arguments
+*  =========
+*
+*  DIN     (input) DOUBLE PRECISION
+*          Input to test for NaN.
+*
+*  =====================================================================
+*
+*  .. External Functions ..
+      LOGICAL DLAISNAN
+      EXTERNAL DLAISNAN
+*  ..
+*  .. Executable Statements ..
+      DISNAN = DLAISNAN(DIN,DIN)
+      RETURN
+      END
