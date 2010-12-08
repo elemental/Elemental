346af30066b13b53c97cd904dc5c87b42567f426
Jack Poulson <jack.poulson@gmail.com>
2010-12-08 11:29:44 -0600

    Rewrote CMake build system to avoid mixing different build types due to CMAKE_CXX_FLAGS not being modified between the different build modes.

--- cmake/Toolchain-BlueGeneP-xl.cmake
+++ cmake/Toolchain-BlueGeneP-xl.cmake
@@ -1,20 +1,16 @@
 set(CMAKE_SYSTEM_NAME BlueGeneP-static)
 
-# Serial compilers for Elemental+PMRRR
-set(CMAKE_C_COMPILER /soft/apps/ibmcmp-aug2010/vac/bg/9.0/bin/bgxlc_r)
-set(CMAKE_CXX_COMPILER /soft/apps/ibmcmp-aug2010/vacpp/bg/9.0/bin/bgxlC_r)
+# We need MPI C and CXX compilers, but only a serial Fortran compiler
+set(CMAKE_C_COMPILER /bgsys/drivers/ppcfloor/comm/bin/mpixlc_r)
+set(CMAKE_CXX_COMPILER /bgsys/drivers/ppcfloor/comm/bin/mpixlcxx_r)
 set(CMAKE_Fortran_COMPILER /soft/apps/ibmcmp-aug2010/xlf/bg/11.1/bin/bgxlf_r) 
-
-# MPI compilers
-set(MPI_CXX_COMPILER /bgsys/drivers/ppcfloor/comm/bin/mpixlcxx_r)
-set(MPI_C_COMPILER /bgsys/drivers/ppcfloor/comm/bin/mpixlc_r)
 
 set(CXX_PURE_DEBUG_FLAGS "-g")
 set(CXX_PURE_RELEASE_FLAGS "-g -O4")
 set(CXX_HYBRID_DEBUG_FLAGS "-g")
 set(CXX_HYBRID_RELEASE_FLAGS "-g -O4")
 
-set(MANUAL_OPENMP_CXX_FLAGS "-qsmp=omp:noauto -qthreaded")
+set(OpenMP_CXX_FLAGS "-qsmp=omp:noauto -qthreaded")
 
 # The remainder of the file is for linking BLAS/LAPACK functionality
 set(ESSL_BASE "/soft/apps/ESSL-4.3.1-1")
