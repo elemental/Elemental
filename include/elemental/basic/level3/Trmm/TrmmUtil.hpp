ff95ae7d9a9e9fdae79efcbc1ce091c2213bddf0
Jack Poulson <jack.poulson@gmail.com>
2012-01-01 20:58:28 -0600

    Removing a straggling declaration inside of elemental::basic and updating the documentation.

--- include/elemental/basic/level3/Trmm/TrmmUtil.hpp
+++ include/elemental/basic/level3/Trmm/TrmmUtil.hpp
@@ -32,7 +32,6 @@
 */
 
 namespace elemental {
-namespace basic {
 namespace internal {
 
 template<typename T> // represents a real or complex ring
@@ -64,6 +63,5 @@ SetDiagonalToOne( DistMatrix<T,MC,MR>& D )
 }
 
 } // internal namespace
-} // basic namespace
 } // elemental namespace
 
