aa5f5c2ca73d010b7308f6aaf5efb5c5fdbbfe3b
Jack Poulson <jack.poulson@gmail.com>
2013-05-21 12:41:50 -0700

    Adding toolchain file for Stampede

--- cmake/toolchains/Stampede-intel-mvapich2-mkl.cmake
+++ cmake/toolchains/Stampede-intel-mvapich2-mkl.cmake
@@ -1,11 +1,11 @@
 # The serial Intel compilers
-set(COMPILER_DIR /opt/apps/intel/11.1/bin/intel64)
+set(COMPILER_DIR /opt/apps/intel/13/composer_xe_2013.2.146/bin/intel64)
 set(CMAKE_C_COMPILER       ${COMPILER_DIR}/icc)
 set(CMAKE_CXX_COMPILER     ${COMPILER_DIR}/icpc)
 set(CMAKE_Fortran_COMPILER ${COMPILER_DIR}/ifort)
 
 # The MPI wrappers for the C and C++ compilers
-set(MPI_COMPILER_DIR /opt/apps/intel11_1/mvapich2/1.6/bin)
+set(MPI_COMPILER_DIR /opt/apps/intel13/mvapich2/1.9/bin)
 set(MPI_C_COMPILER       ${MPI_COMPILER_DIR}/mpicc)
 set(MPI_CXX_COMPILER     ${MPI_COMPILER_DIR}/mpicxx)
 set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
@@ -13,10 +13,10 @@ set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
 set(MPI_C_COMPILE_FLAGS "")
 set(MPI_CXX_COMPILE_FLAGS "")
 set(MPI_Fortran_COMPILE_FLAGS "")
-set(MPI_C_INCLUDE_PATH       /opt/apps/intel11_1/mvapich2/1.6/include)
+set(MPI_C_INCLUDE_PATH       /opt/apps/intel13/mvapich2/1.9/include)
 set(MPI_CXX_INCLUDE_PATH     ${MPI_C_INCLUDE_PATH})
 set(MPI_Fortran_INCLUDE_PATH ${MPI_C_INCLUDE_PATH})
-set(MPI_C_LINK_FLAGS "-Wl,-rpath,/opt/apps/intel/11.1/lib/intel64 -L/opt/apps/intel/11.1/lib/intel64 -Wl,-rpath,/opt/apps/limic2/0.5.4/lib -L/opt/apps/limic2/0.5.4/lib -L/opt/apps/intel11_1/mvapich2/1.6/lib -L/opt/ofed/lib64/")
+set(MPI_C_LINK_FLAGS "-Wl,-rpath,/opt/apps/limic2/0.5.5/lib -L/opt/apps/limic2/0.5.5/lib -L/opt/apps/intel13/mvapich2/1.9/lib -L/opt/ofed/lib64/")
 set(MPI_CXX_LINK_FLAGS ${MPI_C_LINK_FLAGS})
 set(MPI_Fortran_LINK_FLAGS ${MPI_C_LINK_FLAGS})
 set(MPI_BASE_LIBS 
@@ -32,10 +32,4 @@ set(CXX_FLAGS_HYBRIDRELEASE "-O3")
 
 set(OpenMP_CXX_FLAGS "-openmp")
 
-if(CMAKE_BUILD_TYPE MATCHES PureDebug OR
-   CMAKE_BUILD_TYPE MATCHES PureRelease OR
-   NOT CMAKE_BUILD_TYPE)
-  set(MATH_LIBS "-L/opt/apps/intel/11.1/mkl/lib/em64t -lmkl_intel_lp64 -lmkl_sequential -lmkl_core /opt/apps/intel/11.1/lib/intel64/libifcore.a /opt/apps/intel/11.1/lib/intel64/libsvml.a")
-else()
-  set(MATH_LIBS "-L/opt/apps/intel/11.1/mkl/lib/em64t -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -lguide -lpthread /opt/apps/intel/11.1/lib/intel64/libifcore.a /opt/apps/intel/11.1/lib/intel64/libsvml.a")
-endif()
+set(MATH_LIBS "-mkl")
