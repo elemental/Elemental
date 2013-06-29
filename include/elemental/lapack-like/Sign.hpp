8dab6c93253d1761e1f5b05bf4e5e5ea0fd4cd83
Jack Poulson <jack.poulson@gmail.com>
2013-06-29 16:20:25 -0700

    Reorganizing the matrix Sign calculations in order to make them more extensible.

--- include/elemental/lapack-like/Sign.hpp
+++ include/elemental/lapack-like/Sign.hpp
@@ -31,6 +31,108 @@ enum Scaling {
 };
 
 template<typename F>
+inline void
+NewtonStep( const Matrix<F>& X, Matrix<F>& XNew, Scaling scaling=DETERMINANT )
+{
+#ifndef RELEASE
+    CallStackEntry entry("sign::NewtonStep");
+#endif
+    typedef BASE(F) R;
+
+    // Calculate mu while forming XNew := inv(X)
+    R mu;
+    Matrix<int> p;
+    XNew = X;
+    LU( XNew, p );
+    if( scaling == DETERMINANT )
+    {
+        SafeProduct<F> det = determinant::AfterLUPartialPiv( XNew, p );
+        mu = R(1)/Exp(det.kappa);
+    }
+    inverse::AfterLUPartialPiv( XNew, p );
+    if( scaling == FROB_NORM )
+        mu = Sqrt( FrobeniusNorm(XNew)/FrobeniusNorm(X) );
+    else if( scaling == NONE )
+        mu = 1;
+
+    // Overwrite XNew with the new iterate
+    const R halfMu = mu/R(2);
+    const R halfMuInv = R(1)/(2*mu); 
+    Scale( halfMuInv, XNew );
+    Axpy( halfMu, X, XNew );
+}
+
+template<typename F>
+inline void
+NewtonStep
+( const DistMatrix<F>& X, DistMatrix<F>& XNew, Scaling scaling=DETERMINANT )
+{
+#ifndef RELEASE
+    CallStackEntry entry("sign::NewtonStep");
+#endif
+    typedef BASE(F) R;
+
+    // Calculate mu while forming B := inv(X)
+    R mu;
+    DistMatrix<int,VC,STAR> p( X.Grid() );
+    XNew = X;
+    LU( XNew, p );
+    if( scaling == DETERMINANT )
+    {
+        SafeProduct<F> det = determinant::AfterLUPartialPiv( XNew, p );
+        mu = R(1)/Exp(det.kappa);
+    }
+    inverse::AfterLUPartialPiv( XNew, p );
+    if( scaling == FROB_NORM )
+        mu = Sqrt( FrobeniusNorm(XNew)/FrobeniusNorm(X) );
+    else if( scaling == NONE )
+        mu = 1;
+
+    // Overwrite XNew with the new iterate
+    const R halfMu = mu/R(2);
+    const R halfMuInv = R(1)/(2*mu); 
+    Scale( halfMuInv, XNew );
+    Axpy( halfMu, X, XNew );
+}
+
+template<typename F>
+inline void
+NewtonSchulzStep( const Matrix<F>& X, Matrix<F>& XTmp, Matrix<F>& XNew )
+{
+#ifndef RELEASE
+    CallStackEntry entry("sign::NewtonSchulzStep");
+#endif
+    typedef BASE(F) R;
+    const int n = X.Height();
+ 
+    // XTmp := 3I - X^2
+    Identity( XTmp, n, n );
+    Gemm( NORMAL, NORMAL, R(-1), X, X, R(3), XTmp );
+
+    // XNew := 1/2 X XTmp
+    Gemm( NORMAL, NORMAL, R(1)/R(2), X, XTmp, XNew );
+}
+
+template<typename F>
+inline void
+NewtonSchulzStep
+( const DistMatrix<F>& X, DistMatrix<F>& XTmp, DistMatrix<F>& XNew )
+{
+#ifndef RELEASE
+    CallStackEntry entry("sign::NewtonSchulzStep");
+#endif
+    typedef BASE(F) R;
+    const int n = X.Height();
+
+    // XTmp := 3I - X^2
+    Identity( XTmp, n, n );
+    Gemm( NORMAL, NORMAL, R(-1), X, X, R(3), XTmp );
+
+    // XNew := 1/2 X XTmp
+    Gemm( NORMAL, NORMAL, R(1)/R(2), X, XTmp, XNew );
+}
+
+template<typename F>
 inline int
 Newton
 ( Matrix<F>& A, Scaling scaling=DETERMINANT, int maxIts=100, BASE(F) tol=1e-6 )
@@ -39,41 +141,22 @@ Newton
     CallStackEntry entry("sign::Newton");
 #endif
     typedef BASE(F) R;
-    Matrix<int> p;
+    Matrix<F> B;
+    Matrix<F> *X=&A, *XNew=&B;
 
     int it=0;
-    Matrix<F> B( A );
     for( ; it<maxIts; ++it )
     {
-        // Both A and B hold the current iterate
-
-        // Calculate mu while forming B := inv(A)
-        R mu;
-        LU( B, p );
-        if( scaling == DETERMINANT )
-        {
-            SafeProduct<F> det = determinant::AfterLUPartialPiv( B, p );
-            mu = R(1)/Exp(det.kappa);
-        }
-        inverse::AfterLUPartialPiv( B, p );
-        if( scaling == FROB_NORM )
-            mu = Sqrt( FrobeniusNorm(B)/FrobeniusNorm(A) );
-        else if( scaling == NONE )
-            mu = 1;
-
-        // Overwrite B with the new iterate
-        const R halfMu = mu/R(2);
-        const R halfMuInv = R(1)/(2*mu); 
-        Scale( halfMuInv, B );
-        Axpy( halfMu, A, B );
+        // Overwrite XNew with the new iterate
+        NewtonStep( *X, *XNew, scaling );
 
         // Use the difference in the iterates to test for convergence
-        Axpy( R(-1), B, A );
-        const R oneDiff = OneNorm( A );
-        const R oneNew = OneNorm( B );
+        Axpy( R(-1), *XNew, *X );
+        const R oneDiff = OneNorm( *X );
+        const R oneNew = OneNorm( *XNew );
 
-        // Ensure that A holds the current iterate and break if possible
-        A = B;
+        // Ensure that X holds the current iterate and break if possible
+        std::swap( X, XNew );
         if( oneDiff/oneNew <= tol )
             break;
     }
@@ -90,46 +173,29 @@ Newton
     CallStackEntry entry("sign::Newton");
 #endif
     typedef BASE(F) R;
-    DistMatrix<int,VC,STAR> p( A.Grid() );
+    DistMatrix<F> B( A.Grid() );
+    DistMatrix<F> *X=&A, *XNew=&B;
 
     int it=0;
-    DistMatrix<F> B( A );
     for( ; it<maxIts; ++it )
     {
-        // Both A and B hold the current iterate
-
-        // Calculate mu while forming B := inv(A)
-        R mu;
-        LU( B, p );
-        if( scaling == DETERMINANT )
-        {
-            SafeProduct<F> det = determinant::AfterLUPartialPiv( B, p );
-            mu = R(1)/Exp(det.kappa);
-        }
-        inverse::AfterLUPartialPiv( B, p );
-        if( scaling == FROB_NORM )
-            mu = Sqrt( FrobeniusNorm(B)/FrobeniusNorm(A) );
-        else if( scaling == NONE )
-            mu = 1;
-
-        // Overwrite B with the new iterate
-        const R halfMu = mu/R(2);
-        const R halfMuInv = R(1)/(2*mu); 
-        Scale( halfMuInv, B );
-        Axpy( halfMu, A, B );
+        // Overwrite XNew with the new iterate
+        NewtonStep( *X, *XNew, scaling );
 
         // Use the difference in the iterates to test for convergence
-        Axpy( R(-1), B, A );
-        const R oneDiff = OneNorm( A );
-        const R oneNew = OneNorm( B );
+        Axpy( R(-1), *XNew, *X );
+        const R oneDiff = OneNorm( *X );
+        const R oneNew = OneNorm( *XNew );
 
-        // Ensure that A holds the current iterate and break if possible
-        A = B; 
+        // Ensure that X holds the current iterate and break if possible
+        std::swap( X, XNew );
         if( oneDiff/oneNew < tol )
             break;
     }
     return it;
 }
+
+// TODO: NewtonSchulzHybrid which estimates when || X^2 - I ||_2 < 1
 
 } // namespace sign
 } // namespace elem
