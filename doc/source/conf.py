cb8bf9e1f6af1fde8d352824720d9b17c9e6222c
Jack Poulson <jack.poulson@gmail.com>
2013-07-07 15:01:04 -0700

    Using sleaker logo.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -110,7 +110,7 @@ html_theme = 'haiku'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'elemental_90.png'
+html_logo = 'elemental.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -184,7 +184,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'elemental_260.png'
+latex_logo = 'elemental.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
