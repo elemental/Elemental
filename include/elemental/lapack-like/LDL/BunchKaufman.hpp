502e1dd13a5957d6031cd1162008eb7b28ce7040
Jack Poulson <jack.poulson@gmail.com>
2013-09-19 14:18:27 -0700

    Refactoring (no pun intended) Bunch Kaufman implementation.

--- include/elemental/lapack-like/LDL/BunchKaufman.hpp
+++ include/elemental/lapack-like/LDL/BunchKaufman.hpp
@@ -13,7 +13,7 @@
 #include "elemental/blas-like/level1/Scale.hpp"
 #include "elemental/blas-like/level2/Syr.hpp"
 
-// TODO: Reference LAPACK's dsytf2
+// TODO: Reference LAPACK's dsytf2 and zhetf2
 
 namespace elem {
 namespace ldl {
@@ -110,6 +110,95 @@ inline void Swap( Orientation orientation, Matrix<F>& X, Matrix<F>& Y )
     }
 }
 
+template<typename F>
+inline void ApplyPivot
+( Orientation orientation, Matrix<F>& A, int to, int from )
+{
+#ifndef RELEASE
+    CallStackEntry cse("ldl::ApplyPivot");
+    if( orientation == NORMAL )
+        LogicError("Invalid orientation");
+#endif
+    const Int n = A.Height();
+    if( to != from )
+    {
+        // Bottom swap
+        auto aToBot   = ViewRange( A, from+1, to,   n, to+1   );
+        auto aFromBot = ViewRange( A, from+1, from, n, from+1 );
+        Swap( NORMAL, aToBot, aFromBot );
+        // Inner swap
+        auto aToInner   = ViewRange( A, to+1, to,   from,   to+1 );
+        auto aFromInner = ViewRange( A, from, to+1, from+1, from );
+        Swap( orientation, aToInner, aFromInner );
+        // Corner swap
+        if( orientation == ADJOINT )
+            A.Set( from, to, Conj(A.Get(from,to)) );
+        // Diagonal swap
+        {
+            const F value = A.Get(from,from);
+            A.Set( from, from, A.Get(to,to) );
+            A.Set( to,   to,   value        );
+        }
+        // Left swap
+        // NOTE: LAPACK would only swap two entries here if nb=2
+        //       (otherwise no left swap)
+        auto aToLeft   = ViewRange( A, to,   0, to+1,   to );
+        auto aFromLeft = ViewRange( A, from, 0, from+1, to );
+        Swap( NORMAL, aToLeft, aFromLeft );
+    }
+}
+
+// TODO: Add support for Algorithm D. Currently using Algorithm A
+template<typename F>
+inline Int
+ChoosePivot( const Matrix<F>& A, Matrix<Int>& p, Int k, BASE(F) gamma )
+{
+#ifndef RELEASE
+    CallStackEntry cse("ldl::ChoosePivot");
+#endif
+    typedef BASE(F) Real;
+    const Int n = A.Height();
+
+    const F alpha11 = A.Get( k, k );
+    const Real alpha11Abs = Abs(alpha11);
+    auto a21 = LockedViewRange( A, k+1, k, n, k+1 );
+    auto a21Pair = FindMax( a21 );
+    const Int r = (k+1) + a21Pair.index;
+    const Real colMax = a21Pair.value;
+    if( colMax == Real(0) && alpha11Abs == Real(0) )
+        throw SingularMatrixException();
+
+    if( alpha11Abs >= gamma*colMax )
+    {
+        p.Set( k, 0, k );
+    }
+    else
+    {
+        // Find maximum off-diag value in row r (exploit symmetry)
+        auto aLeft   = LockedViewRange( A, r,   0, r+1, r   );
+        auto aBottom = LockedViewRange( A, r+1, r, n,   r+1 );
+        auto leftPair   = FindMax( aLeft );
+        auto bottomPair = FindMax( aBottom );
+        const Real rowMax = Max(leftPair.value,bottomPair.value);
+
+        if( alpha11Abs >= gamma*colMax*(colMax/rowMax) )
+        {
+            p.Set( k, 0, k );
+        }
+        else if( Abs(A.Get(r,r)) >= gamma*rowMax )
+        {
+            p.Set( k, 0, r );
+        }
+        else
+        {
+            p.Set( k,   0, -r );
+            p.Set( k+1, 0, -r );
+        }
+    }
+
+    return p.Get( k, 0 );
+}
+
 // Unblocked sequential BunchKaufman
 // TODO: Better documentation of LAPACK approach to diagonal-block inversion
 template<typename F>
@@ -125,7 +214,6 @@ BunchKaufman
     if( orientation == NORMAL )
         LogicError("Can only perform LDL^T or LDL^H");
 #endif
-    typedef BASE(F) Real;
     const bool conjugate = ( orientation==ADJOINT );
     const Int n = A.Height();
     p.ResizeTo( n, 1 );
@@ -133,83 +221,15 @@ BunchKaufman
     Int k=0;
     while( k < n )
     {
-        const F alpha11 = A.Get( k, k );
-        const Real alpha11Abs = Abs(alpha11);
-        auto a21 = ViewRange( A, k+1, k,   n, k+1 );
-        auto A22 = ViewRange( A, k+1, k+1, n, n   );
-
-        auto a21Pair = FindMax( a21 );
-        const Int r = (k+1) + a21Pair.index;
-        const Real colMax = a21Pair.value;
-        Int from, nb;
-        if( colMax == Real(0) && alpha11Abs == Real(0) )
-        {
-            throw SingularMatrixException();
-        }
-        else if( alpha11Abs >= gamma*colMax )
-        {
-            nb = 1;
-            from = k;
-            p.Set( k, 0, from );
-        }
-        else
-        {
-            // Find maximum off-diag value in row r (exploit symmetry)
-            auto aLeft   = ViewRange( A, r,   0, r+1, r   );
-            auto aBottom = ViewRange( A, r+1, r, n,   r+1 );
-            auto leftPair   = FindMax( aLeft );
-            auto bottomPair = FindMax( aBottom );
-            const Real rowMax = Max(leftPair.value,bottomPair.value);
-
-            if( alpha11Abs >= gamma*colMax*(colMax/rowMax) )
-            {
-                nb = 1;
-                from = k;
-                p.Set( k, 0, from );
-            }
-            else if( Abs(A.Get(r,r)) >= gamma*rowMax )
-            {
-                nb = 1;
-                from = r;
-                p.Set( k, 0, from );
-            }
-            else
-            {
-                nb = 2;
-                from = r;
-                p.Set( k,   0, -from );
-                p.Set( k+1, 0, -from );
-            }
-        }
-
-        // Apply pivots
+        // Determine the pivot (block)
+        const Int pivot = ChoosePivot( A, p, k, gamma );
+        const Int nb   = ( pivot >= 0 ? 1     : 2      );
+        const Int from = ( pivot >= 0 ? pivot : -pivot );
         const Int to = k + (nb-1);
-        if( to != from )
-        {
-            // Bottom swap
-            auto aToBot   = ViewRange( A, from+1, to,   n, to+1   );
-            auto aFromBot = ViewRange( A, from+1, from, n, from+1 );
-            Swap( NORMAL, aToBot, aFromBot );
-            // Inner swap
-            auto aToInner   = ViewRange( A, to+1, to,   from,   to+1 );
-            auto aFromInner = ViewRange( A, from, to+1, from+1, from );
-            Swap( orientation, aToInner, aFromInner );
-            // Corner swap
-            if( conjugate )
-                A.Set( from, to, Conj(A.Get(from,to)) );
-            // Diagonal swap
-            {
-                const F value = A.Get(from,from);
-                A.Set( from, from, A.Get(to,to) );
-                A.Set( to,   to,   value        );
-            }
-            // Left swap
-            // NOTE: LAPACK would only swap two entries here if nb=2
-            //       (otherwise no left swap)
-            auto aToLeft   = ViewRange( A, to,   0, to+1,   to );
-            auto aFromLeft = ViewRange( A, from, 0, from+1, to );
-            Swap( NORMAL, aToLeft, aFromLeft );
-        }
+
+        // Apply the symmetric pivot
+        ApplyPivot( orientation, A, to, from );
+
         if( conjugate )
         {
             // Force the active diagonal entries to be real
@@ -222,13 +242,16 @@ BunchKaufman
         if( nb == 1 )
         {
             const F delta11 = F(1)/A.Get(k,k);
+            auto a21 = ViewRange( A, k+1, k,   n, k+1 );
+            auto A22 = ViewRange( A, k+1, k+1, n, n   );
             Syr( LOWER, -delta11, a21, A22, conjugate );
             Scale( delta11, a21 );
         }
         else
         {
             // Rank-two update
-            // Alternative: compute W21=A21/D11 and then apply A22 -= W21 A21'
+            // Alternative: compute W21=A21/D11 and then apply A22 -= W21 A21',
+            // where A22=A(k+2:end,k+2:end)
             if( conjugate )
             {
                 const F delta11 = A.GetRealPart(k,k);
