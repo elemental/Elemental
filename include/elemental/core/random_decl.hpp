871a5f541548f7ca47c0a8612dfc1701043506d5
Jack Poulson <jack.poulson@gmail.com>
2013-11-13 10:53:55 -0500

    Adding missing declaration for integer random number generation.

--- include/elemental/core/random_decl.hpp
+++ include/elemental/core/random_decl.hpp
@@ -23,6 +23,8 @@ T UnitCell();
 template<typename T=double>
 T Uniform( T a=0, T b=UnitCell<T>() );
 
+Int Uniform( Int a, Int b );
+
 // The complex extension of the normal distribution can actually be quite
 // technical, and so we will use the simplest case, where both the real and
 // imaginary components are independently drawn with the same standard 
