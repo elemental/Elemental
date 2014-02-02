90a76402bd1cc2c14a59cac57cfba78ab9bf7227
Jack Poulson <jack.poulson@gmail.com>
2014-02-01 20:49:10 -0500

    Adding blocked sequential lower reduction to Hessenberg form.

--- include/elemental/lapack-like/condense/Hessenberg/L.hpp
+++ include/elemental/lapack-like/condense/Hessenberg/L.hpp
@@ -11,6 +11,7 @@
 #define ELEM_HESSENBERG_L_HPP
 
 #include "./LUnb.hpp"
+#include "./PanelL.hpp"
 
 namespace elem {
 namespace hessenberg {
@@ -18,9 +19,53 @@ namespace hessenberg {
 template<typename F>
 inline void L( Matrix<F>& A, Matrix<F>& t )
 {
-    DEBUG_ONLY(CallStackEntry cse("hessenberg::L"))
-    // TODO: Blocked algorithm
-    LUnb( A, t );
+    DEBUG_ONLY(
+        CallStackEntry cse("hessenberg::L");
+        // Is this requirement necessary?!?
+        if( t.Viewing() )
+            LogicError("t must not be a view");
+    )
+    const Int n = A.Height();
+    t.Resize( Max(n-1,0), 1 );
+
+    Matrix<F> UB1, V01, VB1, G11;
+
+    const Int bsize = Blocksize();
+    for( Int k=0; k<n-1; k+=bsize )
+    {
+        const Int nb = Min(bsize,n-1-k);
+        auto ABR = ViewRange( A, k,    k,    n, n );
+        auto A22 = ViewRange( A, k+nb, k+nb, n, n );
+
+        auto t1 = View( t, k, 0, nb, 1 );
+        UB1.Resize( n-k, nb );
+        VB1.Resize( n-k, nb );
+        G11.Resize( nb,  nb );
+        hessenberg::PanelL( ABR, t1, UB1, VB1, G11 );
+
+        auto AB0 = ViewRange( A,   k,    0, n,   k  );
+        auto A2R = ViewRange( A,   k+nb, k, n,   n  );
+        auto U21 = ViewRange( UB1, nb,   0, n-k, nb );
+        auto V21 = ViewRange( VB1, nb,   0, n-k, nb );
+
+        // AB0 := AB0 - (UB1 inv(G11)^H UB1^H AB0)
+        //      = AB0 - (UB1 ((AB0^H UB1) inv(G11))^H)
+        // -------------------------------------------
+        Gemm( ADJOINT, NORMAL, F(1), AB0, UB1, V01 );
+        Trsm( RIGHT, UPPER, NORMAL, NON_UNIT, F(1), G11, V01 ); 
+        Gemm( NORMAL, ADJOINT, F(-1), UB1, V01, F(1), AB0 );
+
+        // A2R := (A2R - U21 inv(G11)^H VB1^H)(I - UB1 inv(G11) UB1^H)
+        // -----------------------------------------------------------
+        // A2R := A2R - U21 inv(G11)^H VB1^H
+        // (note: VB1 is overwritten)
+        Trsm( RIGHT, UPPER, NORMAL, NON_UNIT, F(1), G11, VB1 );
+        Gemm( NORMAL, ADJOINT, F(-1), U21, VB1, F(1), A2R );
+        // A2R := A2R - ((A2R UB1) inv(G11)) UB1^H
+        Gemm( NORMAL, NORMAL, F(1), A2R, UB1, F(0), V21 );
+        Trsm( RIGHT, UPPER, NORMAL, NON_UNIT, F(1), G11, V21 );
+        Gemm( NORMAL, ADJOINT, F(-1), V21, UB1, F(1), A2R );
+    }
 }
 
 template<typename F> 
--- include/elemental/lapack-like/condense/Hessenberg/PanelL.hpp
+++ include/elemental/lapack-like/condense/Hessenberg/PanelL.hpp
@@ -7,8 +7,8 @@
    http://opensource.org/licenses/BSD-2-Clause
 */
 #pragma once
-#ifndef ELEM_HESSENBERG_PANELU_HPP
-#define ELEM_HESSENBERG_PANELU_HPP
+#ifndef ELEM_HESSENBERG_PANELL_HPP
+#define ELEM_HESSENBERG_PANELL_HPP
 
 #include ELEM_GEMV_INC
 #include ELEM_TRSV_INC
@@ -20,22 +20,21 @@
 namespace elem {
 namespace hessenberg {
 
-// NOTE: This is an extension into complex arithmetic of the sequential algorithm proposed in:
+// NOTE: This is an extension into complex arithmetic 
+//       (and conjugate-transposition) of the sequential algorithm proposed in:
 //       G. Quintana-Orti and R. van de Geijn,  
 //       "Improving the performance of reduction to Hessenberg form" 
-//       After switching to complex arithmetic, it was more natural to switch 
-//       to lower-triangular matrices in the UT transform.
 
 // NOTE: It would be possible to avoid the space for U if we were more careful
 //       about applying the portion interleaved with the Hessenberg matrix.
 template<typename F>
-inline void PanelU
+inline void PanelL
 ( Matrix<F>& A, Matrix<F>& t, Matrix<F>& U, Matrix<F>& V, Matrix<F>& G )
 {
     const Int nU = U.Width();
     const Int n = A.Height();
     DEBUG_ONLY(
-        CallStackEntry cse("hessenberg::PanelU");
+        CallStackEntry cse("hessenberg::PanelL");
         if( nU >= n )            
             LogicError("V is too wide for the panel factorization");
         if( U.Height() != A.Height() )
@@ -56,13 +55,14 @@ inline void PanelU
 
     for( Int k=0; k<nU; ++k )
     {
-        auto a21      = ViewRange( A, k+1, k,   n,   k+1 );
-        auto alpha21T = ViewRange( A, k+1, k,   k+2, k+1 );
-        auto a21B     = ViewRange( A, k+2, k,   n,   k+1 );
+        auto a12      = ViewRange( A, k,   k+1, k+1, n   );
+        auto alpha12L = ViewRange( A, k,   k+1, k+1, k+2 );
+        auto a12R     = ViewRange( A, k,   k+2, k+1, n   );
         auto A22      = ViewRange( A, k+1, k+1, n,   n   );
-        auto a1       = ViewRange( A, 0,   k,   n,   k+1 );
-        auto A2       = ViewRange( A, 0,   k+1, n,   n   );
+        auto a1       = ViewRange( A, k,   0,   k+1, n   );
+        auto A2       = ViewRange( A, k+1, 0,   n,   n   ); 
 
+        // NOTE: Transposes of the horizontal Householder vectors
         auto U0  = ViewRange( U, 0,   0, n,   k   );
         auto u10 = ViewRange( U, k,   0, k+1, k );
         auto u21 = ViewRange( U, k+1, k, n,   k+1 );
@@ -71,40 +71,44 @@ inline void PanelU
         auto V0 = ViewRange( V, 0, 0, n, k   );
         auto v1 = ViewRange( V, 0, k, n, k+1 );
 
-        auto G00     = ViewRange( G, 0, 0, k,   k   );
-        auto g10     = ViewRange( G, k, 0, k+1, k   );
+        auto G00     = ViewRange( G, 0, 0, k, k   );
+        auto g01     = ViewRange( G, 0, k, k, k+1 );
         auto gamma11 = ViewRange( G, k, k, k+1, k+1 );
 
-        // a1 := (I - U0 inv(G00) U0^H) (a1 - V0 inv(G00)^H u10^H)
+        // a1 := (a1 - u10 inv(G00)^H V0^H) (I - U0 inv(G00) U0^H)
         // -------------------------------------------------------
-        // a1 := a1 - V0 inv(G00)^H u10^H
-        Conjugate( u10, y10 );
-        Trsv( LOWER, ADJOINT, NON_UNIT, G00, y10 );
+        // a1 := conj(a1 - (u10 inv(G00)^H) V0^H)
+        //     = conj(a1 - (V0 (inv(G00) u10^H))^H)
+        //     = conj(a1) - (V0 (inv(G00) u10^H))^T
+        Conjugate( u10, y10 ); 
+        Trsv( UPPER, NORMAL, NON_UNIT, G00, y10 );
+        Conjugate( a1 );
         Gemv( NORMAL, F(-1), V0, y10, F(1), a1 );
-        // a1 := a1 - U0 (inv(G00) (U0^H a1))
-        Gemv( ADJOINT, F(1), U0, a1, F(0), y10 ); 
-        Trsv( LOWER, NORMAL, NON_UNIT, G00, y10 );
+        // a1 := conj(a1) - conj(a1) U0 inv(G00) U0^H
+        //     = conj(a1 - (U0 (inv(G00)^H (U0^H a1^T)))^T)
+        Gemv( ADJOINT, F(1), U0, a1, F(0), y10 );
+        Trsv( UPPER, ADJOINT, NON_UNIT, G00, y10 );
         Gemv( NORMAL, F(-1), U0, y10, F(1), a1 );
+        Conjugate( a1 );
 
         // Find tau and v such that
-        //  / I - tau | 1 | | 1, v^H | \ | alpha21T | = | beta |
-        //  \         | v |            / |     a21B |   |    0 |
-        const F tau = LeftReflector( alpha21T, a21B );
+        // | alpha12L a12R | / I - tau | 1   | | 1 conj(v) | \ = | beta 0 |
+        //                   \         | v^T |               /
+        const F tau = RightReflector( alpha12L, a12R );
         t.Set(k,0,tau);
 
-        // Store u21 := | 1 |
-        //              | v |
-        u21 = a21;
+        // Store u21 := | 1   |
+        //              | v^T |
+        Transpose( a12, u21 );
         u21.Set(0,0,F(1));
 
-        // v1 := A2 u21
+        // v1 := A2^H u21
         Zeros( v1, n, 1 );
-        Gemv( NORMAL, F(1), A2, u21, F(0), v1 );
+        Gemv( ADJOINT, F(1), A2, u21, F(0), v1 );
 
-        // g10 := u21^H U20 = (U20^H u21)^H
-        Zeros( g10, 1, k );
-        Gemv( ADJOINT, F(1), U20, u21, F(0), g10 );
-        Conjugate( g10 );
+        // g01 := U20^H u21
+        Zeros( g01, k, 1 );
+        Gemv( ADJOINT, F(1), U20, u21, F(0), g01 );
         
         // gamma11 := 1/tau
         gamma11.Set(0,0,F(1)/tau);
@@ -114,4 +118,4 @@ inline void PanelU
 } // namespace hessenberg
 } // namespace elem
 
-#endif // ifndef ELEM_HESSENBERG_PANELU_HPP
+#endif // ifndef ELEM_HESSENBERG_PANELL_HPP
