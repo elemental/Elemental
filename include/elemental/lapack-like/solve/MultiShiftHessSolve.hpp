22b526148c09451a5891ac83bc2e06070c631661
Jack Poulson <jpoulson@cc.gatech.edu>
2014-03-05 18:34:50 -0500

    Adding preliminary implementation of a distributed multi-shift Hessenberg solver.

--- include/elemental/lapack-like/solve/MultiShiftHessSolve.hpp
+++ include/elemental/lapack-like/solve/MultiShiftHessSolve.hpp
@@ -101,23 +101,104 @@ UN( F alpha, const Matrix<F>& H, const Matrix<F>& shifts, Matrix<F>& X )
 //       upper-Hessenberg matrix H since whole columns will need to be formed 
 //       on every process and this distribution will keep the communication 
 //       balanced.
-template<typename F,Dist UH,Dist VH,Dist UX,Dist VX>
+template<typename F,Dist UH,Dist VH,Dist VX>
 inline void
 UN
 ( F alpha, const DistMatrix<F,UH,VH>& H, const DistMatrix<F,VX,STAR>& shifts,
-  DistMatrix<F,UX,VX>& X ) 
+  DistMatrix<F,STAR,VX>& X ) 
 {
-    DEBUG_ONLY(CallStackEntry cse("mshs::UN"))
+    DEBUG_ONLY(
+        CallStackEntry cse("mshs::UN");
+        if( shifts.ColAlign() != X.RowAlign() )
+            LogicError("shifts and X are not aligned");
+    )
     Scale( alpha, X );
-
-    const Grid& g = H.Grid();
 
     const Int m = X.Height();
     const Int n = X.Width();
+    const Int nLoc = X.LocalWidth();
+    if( m == 0 )
+        return;
+
+    // Initialize storage for Givens rotations
+    typedef Base<F> Real;
+    const Grid& g = H.Grid();
+    DistMatrix<Real,STAR,VX> C(g);
+    DistMatrix<F,STAR,VX> S(g);
+    C.AlignWith( X );
+    S.AlignWith( X );
+    C.Resize( m, n );
+    S.Resize( m, n );
+
+    // Initialize the workspace for shifted columns of H
+    DistMatrix<F,STAR,VX> W(m,n,g);
+    {
+        auto hLast = LockedView( H, 0, m-1, m, 1 );
+        DistMatrix<F,STAR,STAR> hLast_STAR_STAR( hLast );
+        for( Int jLoc=0; jLoc<nLoc; ++jLoc )
+        {
+            MemCopy( W.Buffer(0,jLoc), hLast_STAR_STAR.LockedBuffer(), m );
+            W.UpdateLocal( m-1, jLoc, -shifts.GetLocal(jLoc,0) );
+        }
+    }
+     
+    // Simulataneously factor and solve against R
+    DistMatrix<F,STAR,STAR> hT_STAR_STAR(g);
     for( Int k=m-1; k>0; --k )
     {
-        LogicError("This routine is not yet written");
+        auto hT = LockedView( H, 0, k-1, k-1, 1 );
+        hT_STAR_STAR = hT;
+        const F etakkm1 = H.Get(k,k-1);
+        const F etakm1km1 = H.Get(k-1,k-1);
+        for( Int jLoc=0; jLoc<nLoc; ++jLoc )
+        {
+            Real c;
+            F s, rho;
+            lapack::ComputeGivens( W.GetLocal(k,jLoc), etakkm1, &c, &s, &rho );
+            C.SetLocal( k, jLoc, c );
+            S.SetLocal( k, jLoc, s );
+
+            X.SetLocal
+            ( k, jLoc, X.GetLocal(k,jLoc)/(c*W.GetLocal(k,jLoc)+s*etakkm1) );
+
+            auto xT = View( X.Matrix(), 0, jLoc, k-1, 1 );
+            auto wT = View( W.Matrix(), 0, jLoc, k-1, 1 );
+
+            const F xc = X.GetLocal(k,jLoc)*c;
+            const F xs  = X.GetLocal(k,jLoc)*s;
+            Axpy( -xc, wT, xT );
+            Axpy( -xs, hT_STAR_STAR.LockedMatrix(), xT );
+
+            Scale( -Conj(s), wT );
+            Axpy( c, hT_STAR_STAR.LockedMatrix(), wT );
+
+            const F mu = shifts.GetLocal( jLoc, 0 );
+            X.UpdateLocal
+            ( k-1, jLoc, -xc*W.GetLocal(k-1,jLoc)-xs*(etakm1km1-mu) );
+            W.SetLocal
+            ( k-1, jLoc, c*(etakm1km1-mu)-Conj(s)*W.GetLocal(k-1,jLoc) );
+        }
     }
+    for( Int jLoc=0; jLoc<nLoc; ++jLoc )
+        X.SetLocal( 0, jLoc, X.GetLocal(0,jLoc)/W.GetLocal(0,jLoc) );
+
+    // Solve against Q
+    Matrix<F> t1(nLoc,1), t2(nLoc,1);
+    for( Int jLoc=0; jLoc<nLoc; ++jLoc )
+        t1.Set( jLoc, 0, X.GetLocal(0,jLoc) );
+    for( Int k=1; k<m; ++k )
+    {
+        for( Int jLoc=0; jLoc<nLoc; ++jLoc )
+        {
+            t2.Set( jLoc, 0, X.GetLocal(k,jLoc) );
+            X.SetLocal( k-1, jLoc, C.GetLocal(k,jLoc)*t1.Get(jLoc,0)+
+                                   S.GetLocal(k,jLoc)*t2.Get(jLoc,0) );
+            t1.Set( jLoc, 0, C.GetLocal(k,jLoc)*t2.Get(jLoc,0)-
+                             Conj(S.GetLocal(k,jLoc))*t1.Get(jLoc,0) );
+        }
+    }
+    for( Int jLoc=0; jLoc<nLoc; ++jLoc )
+        X.SetLocal( m-1, jLoc, t1.Get(jLoc,0) );
 }
 
 // TODO: UT, LN, and LT
