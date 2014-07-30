7826d80fc56f86cda9f706525f41bde1cee35e02
Austin Benson <arbenson@berkeley.edu>
2014-07-30 13:28:12 -0700

    Update toolchain version numbers for Edison.

--- cmake/toolchains/Edison-gnu-mpich-libsci.cmake
+++ cmake/toolchains/Edison-gnu-mpich-libsci.cmake
@@ -4,7 +4,7 @@
 #
 
 # The Cray wrappers
-set(COMPILER_DIR /opt/cray/craype/1.01/bin)
+set(COMPILER_DIR /opt/cray/craype/2.1.2/bin)
 set(CMAKE_C_COMPILER       ${COMPILER_DIR}/cc)
 set(CMAKE_CXX_COMPILER     ${COMPILER_DIR}/CC)
 set(CMAKE_Fortran_COMPILER ${COMPILER_DIR}/ftn)
@@ -28,7 +28,7 @@ set(OpenMP_CXX_FLAGS "-fopenmp")
 #set(MATH_LIBS "/opt/xt-libsci/11.0.06/gnu/46/mc12/lib/libsci_gnu.a;/opt/gcc/4.7.1/snos/lib64/libgfortran.a;-lm")
 #set(MATH_LIBS "/opt/cray/libsci/default/GNU/47/sandybridge/lib/libsci_gnu.a;/opt/gcc/4.7.2/snos/lib64/libgfortran.a;-lm")
 #set(MATH_LIBS "/opt/cray/libsci/default/GNU/47/sandybridge/lib/libsci_gnu.a;/opt/gcc/4.7.2/snos/lib64/libgfortran.a;/opt/cray/xc-sysroot/5.0.15/usr/lib64/libm.a")
-set(MATH_LIBS "/opt/cray/libsci/default/GNU/47/sandybridge/lib/libsci_gnu.a")
+set(MATH_LIBS "/opt/cray/libsci/default/GNU/49/sandybridge/lib/libsci_gnu.a")
 set(CMAKE_FIND_LIBRARY_SUFFIXES .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
 set(BUILD_SHARED_LIBS OFF)
 set(CMAKE_EXE_LINKER_FLAGS "-static")
