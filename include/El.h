062b44a460f665c8d8bebec80c548912b4cdff22
Jack Poulson <jpoulson@cc.gatech.edu>
2014-10-23 20:13:11 -0700

    Adding missing C include of MultiVec.h

--- include/El.h
+++ include/El.h
@@ -23,6 +23,7 @@
 #include "El/core/DistGraph.h"
 #include "El/core/SparseMatrix.h"
 #include "El/core/DistSparseMatrix.h"
+#include "El/core/MultiVec.h"
 #include "El/core/View.h"
 #include "El/core/flame_part/Merge.h"
 #include "El/core/flame_part/Partition.h"
