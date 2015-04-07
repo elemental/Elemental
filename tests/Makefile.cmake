749e42d6e2a8c337f156c6ad588790d6c9f90df9
Jiahao Chen <jiahao@mit.edu>
2015-04-06 18:53:34 -0700

    Add Travis CI support
    Uses MPI test environment from MPI.jl

--- tests/Makefile.cmake
+++ tests/Makefile.cmake
@@ -10,8 +10,8 @@ all: $(TESTS)
 
 %: %.cpp
 	$(CXX) $(EL_COMPILE_FLAGS) $< -o $@ $(EL_LINK_FLAGS) $(EL_LIBS)
-	if [ x$(OS) == xLinux ]; then LD_LIBRARY_PATH=$(LD_LIBRARY_PATH):$(EL_LIB) ./$@; fi
-	if [ x$(OS) == xDarwin ]; then DYLD_LIBRARY_PATH=$(DYLD_LIBRARY_PATH):$(EL_LIB) ./$@; fi
+	if test x$(OS) = xLinux; then LD_LIBRARY_PATH=$(LD_LIBRARY_PATH):$(EL_LIB) ./$@; fi
+	if test x$(OS) = xDarwin; then DYLD_LIBRARY_PATH=$(DYLD_LIBRARY_PATH):$(EL_LIB) ./$@; fi
 	rm $@
 
 clean:
