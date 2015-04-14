2ab434f6c420e3558ceae22ce45af3b1b75d326c
Jake Bolewski <jakebolewski@gmail.com>
2015-04-14 10:54:21 -0700

    update openmpi install in correct travis support script

--- travis/install-mpi.sh
+++ travis/install-mpi.sh
@@ -4,7 +4,12 @@ case $1 in
   mpich2) set -x;
     sudo apt-get install -q mpich2 libmpich2-3 libmpich2-dev;;
   openmpi) set -x;
-    sudo apt-get install openmpi-bin openmpi-dev;;
+    sudo apt-get install alien;
+    wget -q http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.4-1.src.rpm;
+    sudo alien -d ./openmpi-1.8.4-1.src.rpm;
+    sudo dpkg -i ./openmpi-1.8.4-1.deb;
+    rm -f ./openmpi-1.8.4-1.src.rpm;
+    rm -f ./openmpi-1.8.4-1.deb;;
   *)
     echo "Unknown MPI implementation:" $1; exit 1;;
 esac
