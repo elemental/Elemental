8a75f38c2b45e4037e2c4843b5f6ad73c1d0a917
Jack Poulson <poulson@stanford.edu>
2015-04-23 16:17:52 -0700

    Attempting to add proper RPATH support for external metis and parmetis builds

--- external/cmake/metis.cmake
+++ external/cmake/metis.cmake
@@ -32,14 +32,40 @@ else()
     SOURCE_DIR ${METIS_SOURCE_DIR}
     BINARY_DIR ${METIS_BINARY_DIR}
     TMP_DIR    ${METIS_BINARY_DIR}/tmp
-    CMAKE_ARGS -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-               -D CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-               -D BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+    CMAKE_ARGS 
+      -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+      -D CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+      -D BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+      -D CMAKE_MACOSX_RPATH=${CMAKE_MACOSX_RPATH}
+      -D CMAKE_SKIP_BUILD_RPATH=${CMAKE_SKIP_BUILD_RPATH}
+      -D CMAKE_BUILD_WITH_INSTALL_RPATH=${CMAKE_BUILD_WITH_INSTALL_RPATH}
+      -D CMAKE_INSTALL_RPATH_USE_LINK_PATH=${CMAKE_INSTALL_RPATH_USE_LINK_PATH} 
+      -D CMAKE_INSTALL_RPATH=${CMAKE_INSTALL_RPATH}
     INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
     INSTALL_COMMAND ""
     UPDATE_COMMAND "" 
   )
   add_dependencies(External project_metis)
+
+set(CMAKE_MACOSX_RPATH TRUE)
+
+# use, i.e. don't skip the full RPATH for the build tree
+set(CMAKE_SKIP_BUILD_RPATH FALSE)
+
+# when building, don't use the install RPATH already
+# (but later on when installing)
+set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+
+# add the automatically determined parts of the RPATH
+# which point to directories outside the build tree to the install RPATH
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+# the RPATH to be used when installing, but only if it's not a system directory
+list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+if("${isSystemDir}" STREQUAL "-1")
+   SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+endif()
+
 
   # Extract the installation directory
   ExternalProject_Get_Property(project_metis install_dir)
