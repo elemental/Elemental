0fd60015bd80315b758ca589102bbb1461000b48
Jake Bolewski <jakebolewski@gmail.com>
2015-04-24 13:54:21 -0400

    remove unnecessary rpath Cmake logic

--- external/cmake/metis.cmake
+++ external/cmake/metis.cmake
@@ -47,26 +47,6 @@ else()
   )
   add_dependencies(External project_metis)
 
-set(CMAKE_MACOSX_RPATH TRUE)
-
-# use, i.e. don't skip the full RPATH for the build tree
-set(CMAKE_SKIP_BUILD_RPATH FALSE)
-
-# when building, don't use the install RPATH already
-# (but later on when installing)
-set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-
-# add the automatically determined parts of the RPATH
-# which point to directories outside the build tree to the install RPATH
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-
-# the RPATH to be used when installing, but only if it's not a system directory
-list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
-if("${isSystemDir}" STREQUAL "-1")
-   SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-endif()
-
-
   # Extract the installation directory
   ExternalProject_Get_Property(project_metis install_dir)
 
