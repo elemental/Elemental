4436b582baccd27b9c2d8c462695927ad1efb4dc
YingzhouLi <lyzh588@gmail.com>
2015-11-25 15:31:53 -0800

    Updated test file

--- sandbox/test.cpp
+++ sandbox/test.cpp
@@ -6,11 +6,19 @@ int main( int argc, char* argv[] )
     Initialize( argc, argv );
 
     const Int m = Input("--m","matrix height",100);
-    const Int n = Input("--n","matrix width",100);
+    const Int n = Input("--n","matrix width",20);
 
     DistMatrix<double> A;
     Uniform( A, m, n );
-    Display( A, "A" );
+
+    DistMatrix<Int,VR,STAR> p;
+    DistMatrix<double,STAR,VR> Z;
+    QRCtrl<double> ctrl;
+    ctrl.boundRank = true;
+    ctrl.maxRank = 20;
+    ID( A, p, Z, ctrl );
+    if( mpi::WorldRank() == 0 )
+        std::cout << Z.Height() << ", " << Z.Width() << std::endl;
 
     Finalize();
     return 0;
