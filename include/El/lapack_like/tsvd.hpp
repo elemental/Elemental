0139195b0f9a4f80f97c97378acda2cad18ce433
Ryan H. Lewis <ryan.lewis@ayasdi.com>
2016-01-20 17:06:46 -0800

    tsvd.hpp

--- include/El/lapack_like/tsvd.hpp
+++ include/El/lapack_like/tsvd.hpp
@@ -3,10 +3,13 @@
 
 namespace El {
 
+template< typename Real>
+Real CopySign(const Real& x, const Real& y){ return y >= Real(0) ? Abs(x): -Abs(x); }
+
 template< typename F, 
           typename ForwardOperator,
           typename AdjointOperator>
-std::tuple< bool, Base<F>, Base<F> >
+void
 BidiagLanczos(
     Int m,
     Int n, 
@@ -22,9 +25,11 @@ BidiagLanczos(
     std::vector<Base<F>>& muList,
     std::vector<Base<F>>& nuList,
     bool reorthIn,
-    Base<F> tolError){
+    Base<F> tolError,
+    bool partialProject=false){
     typedef Base<F> Real;
     typename DistMatrix<F> DM;
+    const Real eps = limits::Epsilon<Real>();
 
     bool reorthB = reorthIn;
     Int nReorth = 0;
@@ -37,6 +42,8 @@ BidiagLanczos(
     Real beta = betaList[iter];
 
     
+    std::vector< Int> reorth_nu;
+    if( partialProject){ reorth_nu.reserve( steps); }
     DM vOld( v);
     for( Int j = iter+1; j <= iter+steps; ++j)
     {
@@ -46,13 +53,95 @@ BidiagLanczos(
         Axpy( -beta, vOld, v);
         Real alpha = Nrm2(v);
         //run omega recurrence
+        reorth_nu.clear();
         for(Int i = 0; i < j; ++i)
         {
-            
+            Real nu = betaList[i]*muList[i+1] + 
+                      alphaList[i]*muList[i] - beta*nuList[i];
+            nu = (nu + CopySign(tau, nu))/alpha;
+            if( partialProject && Abs(v) > tolError) { 
+                reorth_nu.emplace_back( i);
+            }
+            nuList[i] = nu;
         }
-
+        Real maxElt = *std::max_element( nuList.begin(), nuList.end(), Abs);
+        maxNuList.emplace_back( maxElt);
     }
+    nuList.emplace_back( Real(1));
+    if (reorth_b || reorthNu.size()>0){
+        if( partialProject){
+            for( Int i: reorth_nu){
+                auto vi = V(ALL, IR(i));
+                Axpy( -Dot(vi, v), vi, v);
+                nuList[i] = 2*eps;
+                nReorthVecs++;
+            }
+        } else { //full reorthogalization
+             for( Int i = 0; i < j; ++i){
+                auto vi = V(ALL, IR(i));
+                Axpy( -Dot(vi, v), vi, v);
+                nuList[i] = eps;
+                nReorthVecs++;
+            }
+        }
+        alpha = Nrm2( v);
+        reorth_b = !reorth_b;
+    }
+    alphaList.emplace_back(alpha);
+    Scale(Real(1)/alpha, v);
+    auto vj = V(ALL,IR(j));
+    vj = v;
 
+    //The u step
+    uOld = u;
+    u = v;
+    // apply the operator
+    A(u);
+    Axpy(-alpha, uOld, u);
+    beta = Nrm2( u);
+
+    //compute omega recurrence
+    std::vector< Int> reorth_mu;
+    if( partialProject){ reorth_mu.reserve( steps); }
+    for( Int i = 0; i <= j; ++i){
+        Real mu = alphaList[i]*nuList[i] - alpha*muList[i];
+        if (i > 0){
+            mu += betaList[i-1]*nuList[i-1];
+        }
+        mu = (mu + CopySign(tau, mu))/beta;
+        if( partialProject && Abs(mu) > tolError){
+            reorth_mu.emplace_back( i);
+        }
+        muList[ i] = mu;
+    }
+    Real maxElt = *std::max_element( muList.begin(), muList.end(), Abs);
+    maxMuList.emplace_back( maxElt);
+    muList.emplace_back( 1);
+    
+    if (reorth_b || reorthMu.size()>0){
+        if( partialProject){
+            for( Int i: reorth_mu){
+                auto ui = U(ALL, IR(i));
+                Axpy( -Dot(ui, u), ui, u);
+                muList[i] = 2*eps;
+                nReorthVecs++;
+            }
+        } else { //full reorthogalization
+             for( Int i = 0; i < j; ++i){
+                auto ui = U(ALL, IR(i));
+                Axpy( -Dot(ui, u), ui, u);
+                muList[i] = eps;
+                nReorthVecs++;
+            }
+        }
+        alpha = Nrm2( u);
+        reorth_b = !reorth_b;
+        nReorth++;
+    }
+    betaList.emplace_back(beta);
+    Scale(Real(1)/beta, u);
+    auto uj = U(ALL,IR(j));
+    uj = u;
 }
 
 
@@ -81,7 +170,6 @@ tsvd(
         auto tau = InfinityNorm(A); //TODO: Impl this?
         DM z( g);
         Zeros(z, 1,1);
-        Int cc = Max((Int)5, nVals);
         Int steps = 5;
         Real initNorm = Nrm2(initial_vec);
         DM v( initialVec);
@@ -112,7 +200,13 @@ tsvd(
         std::vector< Real> maxNuList; 
         maxMuList.reserve(maxIter);
         maxNuList.reserve(maxIter);
-       ///TODO: Implement the rest.
+        ///TODO: Implement the rest.
+        BidiagLanczos( m, n, A, AAdj, numSteps, 
+                       tau, alphaList, betaList, U, V, 
+                       muList, nuList, 
+                       reorthIn, tolError);
+        for( 
+
 
 }
 } //end namespace El
