350fa88a67ac2259a8b9137cee3268e7a0b0edb7
Jack Poulson <poulson@stanford.edu>
2016-01-24 18:34:32 -0800

    Adding tests of several datatypes into examples/core/LambdaReduce

--- examples/core/LambdaReduce.cpp
+++ examples/core/LambdaReduce.cpp
@@ -1,45 +1,94 @@
 #include "El.hpp"
 using namespace El;
 
+template<typename T,typename=EnableIf<IsReal<T>>>
+void LambdaTest()
+{
+    if( mpi::Rank() == 0 )
+        Output("Testing with ",TypeName<T>());
+
+    mpi::Comm comm = mpi::COMM_WORLD;
+    const int rank = mpi::Rank( comm );
+
+    const T sumOfRanks =
+      mpi::AllReduce
+      ( T(rank), []( const T& x, const T& y ) { return x+y; },
+        true, comm );
+    if( rank == 0 )
+        Output("sum of ranks=",sumOfRanks);
+
+    const T minRank = 
+      mpi::AllReduce
+      ( T(rank), []( const T& x, const T& y ) { return Min(x,y); },
+        true, comm );
+    if( rank == 0 )
+        Output("minimum rank=",minRank);
+
+    const T maxRank = 
+      mpi::AllReduce
+      ( T(rank), []( const T& x, const T& y ) { return Max(x,y); },
+        true, comm );
+    if( rank == 0 )
+        Output("maximum rank=",maxRank);
+
+    const T prodRank = 
+      mpi::AllReduce
+      ( T(rank+1), []( const T& x, const T& y ) { return x*y; }, true, comm );
+    if( rank == 0 )
+        Output("product of ranks (plus one)=",prodRank);
+}
+
+template<typename T,typename=DisableIf<IsReal<T>>,typename=void>
+void LambdaTest()
+{
+    if( mpi::Rank() == 0 )
+        Output("Testing with ",TypeName<T>());
+
+    mpi::Comm comm = mpi::COMM_WORLD;
+    const int rank = mpi::Rank( comm );
+
+    const T sumOfRanks =
+      mpi::AllReduce
+      ( T(rank), []( const T& x, const T& y ) { return x+y; },
+        true, comm );
+    if( rank == 0 )
+        Output("sum of ranks=",sumOfRanks);
+
+    const T prodRank = 
+      mpi::AllReduce
+      ( T(rank+1), []( const T& x, const T& y ) { return x*y; }, true, comm );
+    if( rank == 0 )
+        Output("product of ranks (plus one)=",prodRank);
+}
+
 int main( int argc, char* argv[] )
 {
     Environment env( argc, argv );
-    mpi::Comm comm = mpi::COMM_WORLD;
-    const Int rank = mpi::Rank(comm);
 
     try
     {
-        const Int sumOfRanks =
-          mpi::AllReduce
-          ( rank,
-            []( const Int& x, const Int& y ) { return x+y; },
-            true, comm );
-        if( rank == 0 )
-            Output("sum of ranks=",sumOfRanks);
+        LambdaTest<Int>();
 
-        const Int minRank = 
-          mpi::AllReduce
-          ( rank,
-            []( const Int& x, const Int& y ) { return Min(x,y); },
-            true, comm );
-        if( rank == 0 )
-            Output("minimum rank=",minRank);
+        LambdaTest<float>();
+        LambdaTest<Complex<float>>();
 
-        const Int maxRank = 
-          mpi::AllReduce
-          ( rank,
-            []( const Int& x, const Int& y ) { return Max(x,y); },
-            true, comm );
-        if( rank == 0 )
-            Output("maximum rank=",maxRank);
+        LambdaTest<double>();
+        LambdaTest<Complex<double>>();
 
-        const Int prodRank = 
-          mpi::AllReduce
-          ( rank+1,
-            []( const Int& x, const Int& y ) { return x*y; },
-            true, comm );
-        if( rank == 0 )
-            Output("product of ranks (plus one)=",prodRank);
+#ifdef EL_HAVE_QD
+        LambdaTest<DoubleDouble>();
+        LambdaTest<QuadDouble>();
+#endif
+
+#ifdef EL_HAVE_QUAD
+        LambdaTest<Quad>();
+        LambdaTest<Complex<Quad>>();
+#endif
+
+#ifdef EL_HAVE_MPC
+        LambdaTest<BigInt>();
+        LambdaTest<BigFloat>();
+#endif
     }
     catch( std::exception& e ) { ReportException(e); }
 
