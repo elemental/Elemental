a9c4a6b4bba5f94aa596aa50b36c78da73213ac5
Jack Poulson <poulson@stanford.edu>
2016-05-25 08:50:03 -0700

    Simplifying local blocksize manipulation

--- src/blas_like/blocksizes.cpp
+++ src/blas_like/blocksizes.cpp
@@ -10,70 +10,25 @@
 #include <El/blas_like.hpp>
 #include <stack>
 
-// TODO: Find a better data structure
-
 namespace {
 using namespace El;
 
 std::stack<Int> blocksizeStack;
 
-// Tuning parameters for basic routines
-Int localSymvIntBlocksize = 64;
-Int localSymvFloatBlocksize = 64;
-Int localSymvDoubleBlocksize = 64;
-Int localSymvComplexFloatBlocksize = 64;
-Int localSymvComplexDoubleBlocksize = 64;
-#ifdef EL_HAVE_QD
-Int localSymvDoubleDoubleBlocksize = 64;
-Int localSymvQuadDoubleBlocksize = 64;
-#endif
-#ifdef EL_HAVE_QUAD
-Int localSymvQuadBlocksize = 64;
-Int localSymvComplexQuadBlocksize = 64;
-#endif
-#ifdef EL_HAVE_MPC
-Int localSymvBigIntBlocksize = 64;
-Int localSymvBigFloatBlocksize = 64;
-Int localSymvComplexBigFloatBlocksize = 64;
-#endif
+template<typename T>
+struct LocalSymvBlocksizeHelper { static Int value; };
+template<typename T>
+Int LocalSymvBlocksizeHelper<T>::value = 64;
 
-Int localTrr2kIntBlocksize = 64;
-Int localTrr2kFloatBlocksize = 64;
-Int localTrr2kDoubleBlocksize = 64;
-Int localTrr2kComplexFloatBlocksize = 64;
-Int localTrr2kComplexDoubleBlocksize = 64;
-#ifdef EL_HAVE_QD
-Int localTrr2kDoubleDoubleBlocksize = 64;
-Int localTrr2kQuadDoubleBlocksize = 64;
-#endif
-#ifdef EL_HAVE_QUAD
-Int localTrr2kQuadBlocksize = 64;
-Int localTrr2kComplexQuadBlocksize = 64;
-#endif
-#ifdef EL_HAVE_MPC
-Int localTrr2kBigIntBlocksize = 64;
-Int localTrr2kBigFloatBlocksize = 64;
-Int localTrr2kComplexBigFloatBlocksize = 64;
-#endif
+template<typename T>
+struct LocalTrrkBlocksizeHelper { static Int value; };
+template<typename T>
+Int LocalTrrkBlocksizeHelper<T>::value = 64;
 
-Int localTrrkIntBlocksize = 64;
-Int localTrrkFloatBlocksize = 64;
-Int localTrrkDoubleBlocksize = 64;
-Int localTrrkComplexFloatBlocksize = 64;
-Int localTrrkComplexDoubleBlocksize = 64;
-#ifdef EL_HAVE_QD
-Int localTrrkDoubleDoubleBlocksize = 64;
-Int localTrrkQuadDoubleBlocksize = 64;
-#endif
-#ifdef EL_HAVE_QUAD
-Int localTrrkQuadBlocksize = 64;
-Int localTrrkComplexQuadBlocksize = 64;
-#endif
-#ifdef EL_HAVE_MPC
-Int localTrrkBigIntBlocksize = 64;
-Int localTrrkBigFloatBlocksize = 64;
-Int localTrrkComplexBigFloatBlocksize = 64;
-#endif
+template<typename T>
+struct LocalTrr2kBlocksizeHelper { static Int value; };
+template<typename T>
+Int LocalTrr2kBlocksizeHelper<T>::value = 64;
 
 }
 
@@ -115,316 +70,43 @@ void EmptyBlocksizeStack()
         ::blocksizeStack.pop();
 }
 
-template<>
-void SetLocalSymvBlocksize<Int>( Int blocksize )
-{ ::localSymvIntBlocksize = blocksize; }
+template<typename T>
+void SetLocalSymvBlocksize( Int blocksize )
+{ LocalSymvBlocksizeHelper<T>::value = blocksize; }
 
-template<>
-void SetLocalSymvBlocksize<float>( Int blocksize )
-{ ::localSymvFloatBlocksize = blocksize; }
+template<typename T>
+Int LocalSymvBlocksize()
+{ return LocalSymvBlocksizeHelper<T>::value; }
 
-template<>
-void SetLocalSymvBlocksize<double>( Int blocksize )
-{ ::localSymvDoubleBlocksize = blocksize; }
+template<typename T>
+void SetLocalTrrkBlocksize( Int blocksize )
+{ LocalTrrkBlocksizeHelper<T>::value = blocksize; }
 
-template<>
-void SetLocalSymvBlocksize<Complex<float>>( Int blocksize )
-{ ::localSymvComplexFloatBlocksize = blocksize; }
+template<typename T>
+Int LocalTrrkBlocksize()
+{ return LocalTrrkBlocksizeHelper<T>::value; }
 
-template<>
-void SetLocalSymvBlocksize<Complex<double>>( Int blocksize )
-{ ::localSymvComplexDoubleBlocksize = blocksize; }
+template<typename T>
+void SetLocalTrr2kBlocksize( Int blocksize )
+{ LocalTrr2kBlocksizeHelper<T>::value = blocksize; }
 
-#ifdef EL_HAVE_QD
-template<>
-void SetLocalSymvBlocksize<DoubleDouble>( Int blocksize )
-{ ::localSymvDoubleDoubleBlocksize = blocksize; }
-template<>
-void SetLocalSymvBlocksize<QuadDouble>( Int blocksize )
-{ ::localSymvQuadDoubleBlocksize = blocksize; }
-#endif
+template<typename T>
+Int LocalTrr2kBlocksize()
+{ return LocalTrr2kBlocksizeHelper<T>::value; }
 
-#ifdef EL_HAVE_QUAD
-template<>
-void SetLocalSymvBlocksize<Quad>( Int blocksize )
-{ ::localSymvQuadBlocksize = blocksize; }
+#define PROTO(T) \
+  template void SetLocalSymvBlocksize<T>( Int blocksize ); \
+  template Int LocalSymvBlocksize<T>(); \
+  template void SetLocalTrrkBlocksize<T>( Int blocksize ); \
+  template Int LocalTrrkBlocksize<T>(); \
+  template void SetLocalTrr2kBlocksize<T>( Int blocksize ); \
+  template Int LocalTrr2kBlocksize<T>(); 
 
-template<>
-void SetLocalSymvBlocksize<Complex<Quad>>( Int blocksize )
-{ ::localSymvComplexQuadBlocksize = blocksize; }
-#endif
-
-#ifdef EL_HAVE_MPC
-template<>
-void SetLocalSymvBlocksize<BigInt>( Int blocksize )
-{ ::localSymvBigIntBlocksize = blocksize; }
-
-template<>
-void SetLocalSymvBlocksize<BigFloat>( Int blocksize )
-{ ::localSymvBigFloatBlocksize = blocksize; }
-
-template<>
-void SetLocalSymvBlocksize<Complex<BigFloat>>( Int blocksize )
-{ ::localSymvComplexBigFloatBlocksize = blocksize; }
-#endif
-
-template<>
-Int LocalSymvBlocksize<Int>()
-{ return ::localSymvIntBlocksize; }
-
-template<>
-Int LocalSymvBlocksize<float>()
-{ return ::localSymvFloatBlocksize; }
-
-template<>
-Int LocalSymvBlocksize<double>()
-{ return ::localSymvDoubleBlocksize; }
-
-template<>
-Int LocalSymvBlocksize<Complex<float>>()
-{ return ::localSymvComplexFloatBlocksize; }
-
-template<>
-Int LocalSymvBlocksize<Complex<double>>()
-{ return ::localSymvComplexDoubleBlocksize; }
-
-#ifdef EL_HAVE_QD
-template<>
-Int LocalSymvBlocksize<DoubleDouble>()
-{ return ::localSymvDoubleDoubleBlocksize; }
-template<>
-Int LocalSymvBlocksize<QuadDouble>()
-{ return ::localSymvQuadDoubleBlocksize; }
-#endif
-
-#ifdef EL_HAVE_QUAD
-template<>
-Int LocalSymvBlocksize<Quad>()
-{ return ::localSymvQuadBlocksize; }
-
-template<>
-Int LocalSymvBlocksize<Complex<Quad>>()
-{ return ::localSymvComplexQuadBlocksize; }
-#endif
-
-#ifdef EL_HAVE_MPC
-template<>
-Int LocalSymvBlocksize<BigInt>()
-{ return ::localSymvBigIntBlocksize; }
-template<>
-Int LocalSymvBlocksize<BigFloat>()
-{ return ::localSymvBigFloatBlocksize; }
-template<>
-Int LocalSymvBlocksize<Complex<BigFloat>>()
-{ return ::localSymvComplexBigFloatBlocksize; }
-#endif
-
-template<>
-void SetLocalTrr2kBlocksize<Int>( Int blocksize )
-{ ::localTrr2kIntBlocksize = blocksize; }
-
-template<>
-void SetLocalTrr2kBlocksize<float>( Int blocksize )
-{ ::localTrr2kFloatBlocksize = blocksize; }
-
-template<>
-void SetLocalTrr2kBlocksize<double>( Int blocksize )
-{ ::localTrr2kDoubleBlocksize = blocksize; }
-
-template<>
-void SetLocalTrr2kBlocksize<Complex<float>>( Int blocksize )
-{ ::localTrr2kComplexFloatBlocksize = blocksize; }
-
-template<>
-void SetLocalTrr2kBlocksize<Complex<double>>( Int blocksize )
-{ ::localTrr2kComplexDoubleBlocksize = blocksize; }
-
-#ifdef EL_HAVE_QD
-template<>
-void SetLocalTrr2kBlocksize<DoubleDouble>( Int blocksize )
-{ ::localTrr2kDoubleDoubleBlocksize = blocksize; }
-
-template<>
-void SetLocalTrr2kBlocksize<QuadDouble>( Int blocksize )
-{ ::localTrr2kQuadDoubleBlocksize = blocksize; }
-#endif
-
-#ifdef EL_HAVE_QUAD
-template<>
-void SetLocalTrr2kBlocksize<Quad>( Int blocksize )
-{ ::localTrr2kQuadBlocksize = blocksize; }
-
-template<>
-void SetLocalTrr2kBlocksize<Complex<Quad>>( Int blocksize )
-{ ::localTrr2kComplexQuadBlocksize = blocksize; }
-#endif
-
-#ifdef EL_HAVE_MPC
-template<>
-void SetLocalTrr2kBlocksize<BigInt>( Int blocksize )
-{ ::localTrr2kBigIntBlocksize = blocksize; }
-template<>
-void SetLocalTrr2kBlocksize<BigFloat>( Int blocksize )
-{ ::localTrr2kBigFloatBlocksize = blocksize; }
-template<>
-void SetLocalTrr2kBlocksize<Complex<BigFloat>>( Int blocksize )
-{ ::localTrr2kComplexBigFloatBlocksize = blocksize; }
-#endif
-
-template<>
-Int LocalTrr2kBlocksize<Int>()
-{ return ::localTrr2kIntBlocksize; }
-
-template<>
-Int LocalTrr2kBlocksize<float>()
-{ return ::localTrr2kFloatBlocksize; }
-
-template<>
-Int LocalTrr2kBlocksize<double>()
-{ return ::localTrr2kDoubleBlocksize; }
-
-template<>
-Int LocalTrr2kBlocksize<Complex<float>>()
-{ return ::localTrr2kComplexFloatBlocksize; }
-
-template<>
-Int LocalTrr2kBlocksize<Complex<double>>()
-{ return ::localTrr2kComplexDoubleBlocksize; }
-
-#ifdef EL_HAVE_QD
-template<>
-Int LocalTrr2kBlocksize<DoubleDouble>()
-{ return ::localTrr2kDoubleDoubleBlocksize; }
-
-template<>
-Int LocalTrr2kBlocksize<QuadDouble>()
-{ return ::localTrr2kQuadDoubleBlocksize; }
-#endif
-
-#ifdef EL_HAVE_QUAD
-template<>
-Int LocalTrr2kBlocksize<Quad>()
-{ return ::localTrr2kQuadBlocksize; }
-
-template<>
-Int LocalTrr2kBlocksize<Complex<Quad>>()
-{ return ::localTrr2kComplexQuadBlocksize; }
-#endif
-
-#ifdef EL_HAVE_MPC
-template<>
-Int LocalTrr2kBlocksize<BigInt>()
-{ return ::localTrr2kBigIntBlocksize; }
-template<>
-Int LocalTrr2kBlocksize<BigFloat>()
-{ return ::localTrr2kBigFloatBlocksize; }
-template<>
-Int LocalTrr2kBlocksize<Complex<BigFloat>>()
-{ return ::localTrr2kComplexBigFloatBlocksize; }
-#endif
-
-template<>
-void SetLocalTrrkBlocksize<Int>( Int blocksize )
-{ ::localTrrkIntBlocksize = blocksize; }
-
-template<>
-void SetLocalTrrkBlocksize<float>( Int blocksize )
-{ ::localTrrkFloatBlocksize = blocksize; }
-
-template<>
-void SetLocalTrrkBlocksize<double>( Int blocksize )
-{ ::localTrrkDoubleBlocksize = blocksize; }
-
-template<>
-void SetLocalTrrkBlocksize<Complex<float>>( Int blocksize )
-{ ::localTrrkComplexFloatBlocksize = blocksize; }
-
-template<>
-void SetLocalTrrkBlocksize<Complex<double>>( Int blocksize )
-{ ::localTrrkComplexDoubleBlocksize = blocksize; }
-
-#ifdef EL_HAVE_QD
-template<>
-void SetLocalTrrkBlocksize<DoubleDouble>( Int blocksize )
-{ ::localTrrkDoubleDoubleBlocksize = blocksize; }
-
-template<>
-void SetLocalTrrkBlocksize<QuadDouble>( Int blocksize )
-{ ::localTrrkQuadDoubleBlocksize = blocksize; }
-#endif
-
-#ifdef EL_HAVE_QUAD
-template<>
-void SetLocalTrrkBlocksize<Quad>( Int blocksize )
-{ ::localTrrkQuadBlocksize = blocksize; }
-
-template<>
-void SetLocalTrrkBlocksize<Complex<Quad>>( Int blocksize )
-{ ::localTrrkComplexQuadBlocksize = blocksize; }
-#endif
-
-#ifdef EL_HAVE_MPC
-template<>
-void SetLocalTrrkBlocksize<BigInt>( Int blocksize )
-{ ::localTrrkBigIntBlocksize = blocksize; }
-template<>
-void SetLocalTrrkBlocksize<BigFloat>( Int blocksize )
-{ ::localTrrkBigFloatBlocksize = blocksize; }
-template<>
-void SetLocalTrrkBlocksize<Complex<BigFloat>>( Int blocksize )
-{ ::localTrrkComplexBigFloatBlocksize = blocksize; }
-#endif
-
-template<>
-Int LocalTrrkBlocksize<Int>()
-{ return ::localTrrkIntBlocksize; }
-
-template<>
-Int LocalTrrkBlocksize<float>()
-{ return ::localTrrkFloatBlocksize; }
-
-template<>
-Int LocalTrrkBlocksize<double>()
-{ return ::localTrrkDoubleBlocksize; }
-
-template<>
-Int LocalTrrkBlocksize<Complex<float>>()
-{ return ::localTrrkComplexFloatBlocksize; }
-
-template<>
-Int LocalTrrkBlocksize<Complex<double>>()
-{ return ::localTrrkComplexDoubleBlocksize; }
-
-#ifdef EL_HAVE_QD
-template<>
-Int LocalTrrkBlocksize<DoubleDouble>()
-{ return ::localTrrkDoubleDoubleBlocksize; }
-
-template<>
-Int LocalTrrkBlocksize<QuadDouble>()
-{ return ::localTrrkQuadDoubleBlocksize; }
-#endif
-
-#ifdef EL_HAVE_QUAD
-template<>
-Int LocalTrrkBlocksize<Quad>()
-{ return ::localTrrkQuadBlocksize; }
-
-template<>
-Int LocalTrrkBlocksize<Complex<Quad>>()
-{ return ::localTrrkComplexQuadBlocksize; }
-#endif
-
-#ifdef EL_HAVE_MPC
-template<>
-Int LocalTrrkBlocksize<BigInt>()
-{ return ::localTrrkBigIntBlocksize; }
-template<>
-Int LocalTrrkBlocksize<BigFloat>()
-{ return ::localTrrkBigFloatBlocksize; }
-template<>
-Int LocalTrrkBlocksize<Complex<BigFloat>>()
-{ return ::localTrrkComplexBigFloatBlocksize; }
-#endif
+#define EL_ENABLE_DOUBLEDOUBLE
+#define EL_ENABLE_QUADDOUBLE
+#define EL_ENABLE_QUAD
+#define EL_ENABLE_BIGINT
+#define EL_ENABLE_BIGFLOAT
+#include <El/macros/Instantiate.h>
 
 } // namespace El
