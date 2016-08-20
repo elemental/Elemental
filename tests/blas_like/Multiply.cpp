adf8f2cb4986e6d6c97ca982e414143de1449ef7
Ryan H. Lewis <me@ryanlewis.net>
2016-08-20 16:55:37 -0700

    added a basic unit test.

--- tests/blas_like/Multiply.cpp
+++ tests/blas_like/Multiply.cpp
@@ -18,64 +18,36 @@ void TestMultiply(Int m, Int n=100)
     DEBUG_ONLY(CallStackEntry cse("TestMultiply"))
     SparseMatrix<T> A;
     Identity( A, m, m);
-    Matrix<T> B;
+    Matrix<T> B( m, n);
     Bernoulli( B, m, n);
     auto G = A.Graph();
-    Matrix<T> C,D;
-    Reshape(m, n, C);
-    Reshape(m, n, D);
-    Multiply(NORMAL, 1.0, A, B, 0.0, C);
-    Multiply(NORMAL, 1.0, G, B, 0.0, D);
+    Matrix<T> C(m,n),D(m,n);
+    Multiply(NORMAL, T(1.0), A, B, T(0.0), C);
+    Multiply(NORMAL, T(1.0), G, B, T(0.0), D);
     Axpy(-1.0, C, D);
     auto nrm = FrobeniusNorm(D);
+    std::cout << "error = " << nrm << std::endl;
     if( nrm > 1e-16){ throw std::runtime_error("Sparse(I)*x != Graph(I)*x"); }
 }
 
-template<typename T>
-void TestMultiplyDiagonal(Int m, Int n=100)
-{
-    DEBUG_ONLY(CallStackEntry cse("TestMultiplyDiagonal"))
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    // values near the mean are the most likely
-    // standard deviation affects the dispersion of generated values from the mean
-    std::normal_distribution<> d(5,2);
-    SparseMatrix<T> A;
-    Laplacian( A, m);
-    Matrix<T> B;
-    Bernoulli( B, m, n);
-    auto G = A.Graph();
-    Matrix<T> C,D;
-    Reshape(m, n, C);
-    Reshape(m, n, D);
-    Multiply(NORMAL, 1.0, A, B, 0.0, C);
-    Multiply(NORMAL, 1.0, G, B, 0.0, D);
-    Axpy(-1.0, C, D);
-    auto nrm = FrobeniusNorm(D);
-    if( nrm > 1e-16){ throw std::runtime_error("Sparse(J)*x != Graph(J)*x"); }
-}
-
 void RunTests( Int m){
-  TestMultiply<float>( m);
   TestMultiply<double>( m);
-  TestMultiplyDiagonal<float>( m);
-  TestMultiplyDiagonal<double>( m);	
+  //List all the types here..
 }
 
 int main( int argc, char* argv[] )
 {
     Environment env( argc, argv );
     mpi::Comm comm = mpi::COMM_WORLD;
-
     try
     {
 	Int m = 1;
 	for(int e = 1; e < 4; ++e){	
-		m *= 10;
+		m *= 10;	
+		std::cout << " m = " << m << std::endl;
 		RunTests( m);
 	}
     }
     catch( exception& e ) { ReportException(e); }
-
     return 0;
 }
