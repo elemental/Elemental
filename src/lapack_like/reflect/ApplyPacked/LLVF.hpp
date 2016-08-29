85572b9c79fab694457bf133e4b01289d4cdcd21
Jack Poulson <jack.poulson@gmail.com>
2016-08-29 08:54:12 -0700

    Adding an unblocked implementation of the LLVF branch of ApplyPackedReflectors. These changes should soon be propagated through the other options as part of addressing Issue #141.

--- src/lapack_like/reflect/ApplyPacked/LLVF.hpp
+++ src/lapack_like/reflect/ApplyPacked/LLVF.hpp
@@ -32,9 +32,52 @@ namespace apply_packed_reflectors {
 //
 
 template<typename F> 
-void LLVF
+void LLVFUnblocked
 ( Conjugation conjugation,
-  Int offset, 
+  Int offset,
+  const Matrix<F>& H,
+  const Matrix<F>& householderScalars,
+        Matrix<F>& A )
+{
+    DEBUG_CSE
+    DEBUG_ONLY(
+      if( H.Height() != A.Height() )
+          LogicError("A and H must be the same height");
+    )
+    const Int m = H.Height();
+    const Int diagLength = H.DiagonalLength(offset);
+    DEBUG_ONLY(
+      if( householderScalars.Height() != diagLength )
+          LogicError
+          ("householderScalars must be the same length as H's offset diag.");
+    )
+    Matrix<F> hPanCopy, z;
+
+    const Int iOff = ( offset>=0 ? 0      : -offset );
+    const Int jOff = ( offset>=0 ? offset : 0       );
+
+    for( Int k=0; k<diagLength; ++k )
+    {
+        const Int ki = k+iOff;
+        const Int kj = k+jOff;
+
+        auto hPan = H( IR(ki,m),   IR(kj) );
+        auto ABot = A( IR(ki,m),   ALL    );
+        const F tau = householderScalars(k);
+        const F gamma = ( conjugation ? Conj(tau) : tau );
+
+        hPanCopy = hPan;
+        hPanCopy(0) = 1;
+
+        Gemv( ADJOINT, F(1), ABot, hPanCopy, z );
+        Ger( -gamma, hPanCopy, z, ABot );
+    }
+}
+
+template<typename F> 
+void LLVFBlocked
+( Conjugation conjugation,
+  Int offset,
   const Matrix<F>& H,
   const Matrix<F>& householderScalars,
         Matrix<F>& A )
@@ -83,23 +126,45 @@ void LLVF
 template<typename F> 
 void LLVF
 ( Conjugation conjugation,
+  Int offset,
+  const Matrix<F>& H,
+  const Matrix<F>& householderScalars,
+        Matrix<F>& A )
+{
+    DEBUG_CSE
+    const Int numRHS = A.Width();
+    const Int blocksize = Blocksize(); 
+    if( numRHS < blocksize )
+    {
+        LLVFUnblocked( conjugation, offset, H, householderScalars, A );
+    }
+    else
+    {
+        LLVFBlocked( conjugation, offset, H, householderScalars, A );
+    }
+}
+
+template<typename F> 
+void LLVFUnblocked
+( Conjugation conjugation,
   Int offset, 
-  const ElementalMatrix<F>& HPre,
+  const ElementalMatrix<F>& H,
   const ElementalMatrix<F>& householderScalarsPre, 
         ElementalMatrix<F>& APre )
 {
     DEBUG_CSE
     DEBUG_ONLY(
-      if( HPre.Height() != APre.Height() )
+      if( H.Height() != APre.Height() )
           LogicError("A and H must be the same height");
-      AssertSameGrids( HPre, householderScalarsPre, APre );
+      AssertSameGrids( H, householderScalarsPre, APre );
     )
 
-    DistMatrixReadProxy<F,F,MC,MR> HProx( HPre );
-    DistMatrixReadProxy<F,F,MC,STAR>
+    // We gather the entire set of Householder scalars at the start rather than
+    // continually paying the latency cost of the broadcasts in a 'Get' call
+    DistMatrixReadProxy<F,F,STAR,STAR>
       householderScalarsProx( householderScalarsPre );
+
     DistMatrixReadWriteProxy<F,F,MC,MR> AProx( APre );
-    auto& H = HProx.GetLocked();
     auto& householderScalars = householderScalarsProx.GetLocked();
     auto& A = AProx.Get();
 
@@ -111,6 +176,67 @@ void LLVF
           ("householderScalars must be the same length as H's offset diag.");
     )
     const Grid& g = H.Grid();
+    auto hPan = unique_ptr<ElementalMatrix<F>>( H.Construct(g,H.Root()) );
+    DistMatrix<F,MC,  STAR> hPan_MC_STAR(g);
+    DistMatrix<F,MR,STAR> z_MR_STAR(g);
+    DistMatrix<F,VR,STAR> z_VR_STAR(g);
+
+    const Int iOff = ( offset>=0 ? 0      : -offset );
+    const Int jOff = ( offset>=0 ? offset : 0       );
+
+    for( Int k=0; k<diagLength; ++k )
+    {
+        const Int ki = k+iOff;
+        const Int kj = k+jOff;
+
+        LockedView( *hPan, H, IR(ki,m), IR(kj) );
+        auto ABot = A( IR(ki,m), ALL );
+        const F tau = householderScalars.GetLocal( k, 0 );
+        const F gamma = ( conjugation ? Conj(tau) : tau );
+
+        hPan_MC_STAR.AlignWith( ABot );
+        Copy( *hPan, hPan_MC_STAR );
+        hPan_MC_STAR.Set( 0, 0, F(1) );
+
+        z_MR_STAR.AlignWith( ABot );
+        Zeros( z_MR_STAR, ABot.Width(), 1 );
+        LocalGemv( ADJOINT, F(1), ABot, hPan_MC_STAR, F(0), z_MR_STAR );
+        El::AllReduce( z_MR_STAR.Matrix(), ABot.ColComm() );
+
+        LocalGer( -gamma, hPan_MC_STAR, z_MR_STAR, ABot );
+    }
+}
+
+template<typename F> 
+void LLVFBlocked
+( Conjugation conjugation,
+  Int offset, 
+  const ElementalMatrix<F>& H,
+  const ElementalMatrix<F>& householderScalarsPre, 
+        ElementalMatrix<F>& APre )
+{
+    DEBUG_CSE
+    DEBUG_ONLY(
+      if( H.Height() != APre.Height() )
+          LogicError("A and H must be the same height");
+      AssertSameGrids( H, householderScalarsPre, APre );
+    )
+
+    DistMatrixReadProxy<F,F,MC,STAR>
+      householderScalarsProx( householderScalarsPre );
+    DistMatrixReadWriteProxy<F,F,MC,MR> AProx( APre );
+    auto& householderScalars = householderScalarsProx.GetLocked();
+    auto& A = AProx.Get();
+
+    const Int m = H.Height();
+    const Int diagLength = H.DiagonalLength(offset);
+    DEBUG_ONLY(
+      if( householderScalars.Height() != diagLength )
+          LogicError
+          ("householderScalars must be the same length as H's offset diag.");
+    )
+    const Grid& g = H.Grid();
+    auto HPan = unique_ptr<ElementalMatrix<F>>( H.Construct(g,H.Root()) );
     DistMatrix<F> HPanCopy(g);
     DistMatrix<F,VC,  STAR> HPan_VC_STAR(g);
     DistMatrix<F,MC,  STAR> HPan_MC_STAR(g);
@@ -122,17 +248,18 @@ void LLVF
     const Int jOff = ( offset>=0 ? offset : 0       );
 
     const Int bsize = Blocksize();
+
     for( Int k=0; k<diagLength; k+=bsize )
     {
         const Int nb = Min(bsize,diagLength-k);
         const Int ki = k+iOff;
         const Int kj = k+jOff;
 
-        auto HPan = H( IR(ki,m),   IR(kj,kj+nb) );
-        auto ABot = A( IR(ki,m),   ALL          );
+        LockedView( *HPan, H, IR(ki,m), IR(kj,kj+nb) );
+        auto ABot = A( IR(ki,m), ALL );
         auto householderScalars1 = householderScalars( IR(k,k+nb), ALL );
 
-        HPanCopy = HPan;
+        Copy( *HPan, HPanCopy );
         MakeTrapezoidal( LOWER, HPanCopy );
         FillDiagonal( HPanCopy, F(1) );
 
@@ -159,6 +286,31 @@ void LLVF
 
         Z_STAR_MR = Z_STAR_VR;
         LocalGemm( NORMAL, NORMAL, F(-1), HPan_MC_STAR, Z_STAR_MR, F(1), ABot );
+    }
+}
+
+template<typename F> 
+void LLVF
+( Conjugation conjugation,
+  Int offset,
+  const ElementalMatrix<F>& H,
+  const ElementalMatrix<F>& householderScalars,
+        ElementalMatrix<F>& A )
+{
+    DEBUG_CSE
+    const Int numRHS = A.Width();
+    const Int blocksize = Blocksize();
+    // It is not clear that switching to the unblocked implementation is as much
+    // of a win as in the sequential case, as there is additional latency
+    // overhead due to increasing the number of reductions by a factor of 
+    // blocksize.
+    if( numRHS < blocksize )
+    {
+        LLVFUnblocked( conjugation, offset, H, householderScalars, A );
+    }
+    else
+    {
+        LLVFBlocked( conjugation, offset, H, householderScalars, A );
     }
 }
 
