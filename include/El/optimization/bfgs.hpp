53a0da2d24d782874a23df9b012e93b32600fd13
Ryan H. Lewis <me@ryanlewis.net>
2016-11-13 14:54:56 -0800

    trying to get the lineSearch routine with interpolation written correctly.

--- include/El/optimization/bfgs.hpp
+++ include/El/optimization/bfgs.hpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2009-2016, Ryan H. Lewis
+   Copyright (c) 2016, Ryan H. Lewis
    All rights reserved.
 
    This file is part of Elemental and is under the BSD 2-Clause License, 
@@ -13,19 +13,30 @@
 
 namespace El {
 /***
- * Performs a quadratic interpolation
+ * Finds the min of the quadratic interpolation
+ * phi_q(alpha) = a*alpha^2 + b*alpha + c
+ * where
+ * a =[fval_low -f0 - alpha_low*f0_dash]/alpha_low^2
+ * b = f0_dash
+ * c = f0;
+ * then the minimizer is:
  */
 template< typename T>
-T QuadraticInterpolate( T alpha_low, T alpha_high, T f0_dash, T f0, T fval_low, T flow_dash)
-{
-    return (f0_dash*alpha_low*alpha_low)/(T(2)*(fval_low - f0 - f0_dash*alpha_low));
+T MinQuadraticInterpolate( T alpha_low, T  f0_dash, T f0, T fval_low) {
+        return (f0_dash * alpha_low * alpha_low) / (T(-2) * (fval_low - f0 - f0_dash * alpha_low));
 }
+ /***
+ * Finds the min of the cubic interpolation
+ * phi_q(alpha) = a*alpha^3 + b*alpha^2 + alpha*f0_dash + f0
+ * a and b are too complicated to encode here, please follow the source code.
+ *
+ */
 template< typename T>
-T CubicInterpolate(T f0, T f0_dash, T f_alpha0, T f_alpha1, T alpha_0, T alpha_1){
-    T alpha_0_sq = alpha_0*alpha_0;
-    T alpha_0_cb = alpha_0*alpha_0*alpha_0;
-    T alpha_1_sq = alpha_1*alpha_1;
-    T alpha_1_cb = alpha_1*alpha_1*alpha_1;
+T MinCubicInterpolate(T f0, T f0_dash, T alpha_0, T f_alpha0, T alpha_1, T f_alpha1){
+    T alpha_0_sq = alpha_0*alpha_0; //alpha_0^2
+    T alpha_0_cb = alpha_0*alpha_0*alpha_0; //alpha_0^3
+    T alpha_1_sq = alpha_1*alpha_1; //alpha_1^2
+    T alpha_1_cb = alpha_1*alpha_1*alpha_1; //alpha_1^3
     T x = f_alpha1 - f0 - f0_dash*alpha_1;
     T y = f_alpha0 - f0 - f0_dash*alpha_0;
     T a = alpha_0_sq*x - alpha_1_sq*y;
@@ -67,21 +78,25 @@ T Zoom( const Function& f, const Gradient& gradient,
     while (alpha_high - alpha_low > T(10)*limits::Epsilon<Base<T>>())
     {
         // BEGIN Interpolation
-        alpha = QuadraticInterpolate(alpha_low, alpha_high, f0_dash, f0, fval_low, flow_dash);
+        /**
+         * The treatment of the interpolation step of zoom is somewhat vague. We have chosen to build a quadratic interpolant,
+         * and if sufficient decrease is not met, we then follow up by a cubic interpolant involving the minimizer of the quadratic.
+         */
+        alpha = MinQuadraticInterpolate(alpha_low, f0_dash, f0, fval_low);
         x_j = x0;
         Axpy(alpha, p, x_j);
         T fval = f(x_j);
-        while ( fval > f0 + c1*alpha*f0_dash)
+        if ( fval > f0 + c1*alpha*f0_dash)
         {
-            alpha = CubicInterpolate(f0, f0_dash, flow_dash, fval, alpha_low, alpha);
+            alpha = MinCubicInterpolate(f0, f0_dash, alpha_low, fval_low, alpha, fval);
             x_j = x0;
             Axpy(alpha, p, x_j);
             fval = f(x_j);
         }
         // End Interpolation
-        x_j = x0;
-        Axpy(alpha, p, x_j);
-        fval = f(x_j);
+        //x_j = x0;
+        //Axpy(alpha, p, x_j);
+        //fval = f(x_j);
         if ((fval > f0 + c1*alpha*f0_dash) || (fval >= fval_low)){
             alpha_high = alpha;
         }else { //alpha certainly satisfies sufficient decrease
@@ -126,7 +141,9 @@ T lineSearch( const Function& f, const Gradient& gradient,
               T c1=Pow(limits::Epsilon< Base<T> >(), Base<T>(1)/Base<T>(4)), T c2=T(9)/T(10))
     {
 
-        if( c1 > c2) { std::swap(c1, c2); }
+        if(c1 > c2) {
+            throw ArgException("c1 must be less than c2");
+        }
 
         T f0 = f(x0);
         DistMatrix<T> g2(D, 1, x0.Grid());
@@ -138,7 +155,7 @@ T lineSearch( const Function& f, const Gradient& gradient,
         T fvalPrev_dash = f0_dash;
         T fval(0);
         DistMatrix<T> x_candidate(x0);
-        //We is maintain that alpha_prev < alpha
+        // We is maintain that alpha_prev < alpha
         for(std::size_t iter = 0; iter < maxIter; ++iter)
         {
             //We work out f(x + alpha*p)
@@ -153,18 +170,18 @@ T lineSearch( const Function& f, const Gradient& gradient,
             {
                 return Zoom(f, gradient, f0, f0_dash, fvalPrev_dash, fvalPrev, x0, p, alpha_prev, alpha, c1, c2);
             }
-            //x_candidate satsifies condition (1)
+            // x_candidate satsifies condition (1)
             gradient( x_candidate, g2);
             auto fval_dash = Dot(p, g2);
-            //If we satisfy condition (2) then terminate.
+            // If we satisfy condition (2) then terminate.
             if(  Abs(fval_dash) <= -c2*f0_dash)
             {
                 return alpha;
             }
-            //Otherwise the curvature condition is violated.
-            //If p'g2 > 0 then we are no longer in a descent direction
-            //So again the interval (alpha_prev,alpha) must contain a value
-            //Which satisfies the curvature conditions.
+            // Otherwise the curvature condition is violated.
+            // If p'g2 > 0 then we are no longer in a descent direction
+            // So again the interval (alpha_prev,alpha) must contain a value
+            // Which satisfies the curvature conditions.
             if( fval_dash > 0)
             {
                 return Zoom(f, gradient, f0, f0_dash, fval, fval_dash, x0, p, alpha, alpha_prev, c1, c2);
@@ -185,8 +202,8 @@ template< typename T>
 struct HessianInverseOperator {
 
     typedef DistMatrix<T> DMatrix;
-    typedef std::tuple< T, DMatrix, DMatrix, DMatrix, T> Update;
-    typedef std::vector< Update > Hessian_updates;
+    typedef std::tuple<T, DMatrix, DMatrix, DMatrix, T> Update;
+    typedef std::vector<Update> Hessian_updates;
     /**
      * This method applies y = H_k*x, eg. solves B_ky = x;
      * (H_k)x = z + alpha*(s'x)s - beta*H_{k-1}y- rho*s
@@ -201,12 +218,12 @@ struct HessianInverseOperator {
     {
         //Initially this is just the identity matrix;
         //H_0 = I, so H_0*x = x;
-        if (_hessian_data.size() == 0) { return x; }
+        if (hessian_data.size() == 0) { return x; }
         DistMatrix<T> z( x);
 
-        //we maintain that z = H_{k-1}*x
+        // We maintain that z = H_{k-1}*x
         // H_kx = z + alpha*(s'x)s - (s'x)/(s'y)H_{k-1}y  (y'z)/(s'y)*s]/(s'y)
-        for( auto& rank_two_update_data: _hessian_data)
+        for( auto& rank_two_update_data: hessian_data)
         {
             auto alpha = std::get<0>(rank_two_update_data);
             const auto& s = std::get<1>(rank_two_update_data);
@@ -247,19 +264,19 @@ struct HessianInverseOperator {
         auto Hy = (*this)*y;
         auto yHy = Dot(Hy,y);
         T alpha = (sy + yHy)/(sy*sy);
-        _hessian_data.emplace_back( alpha, s, y, Hy, sy);
+        hessian_data.emplace_back( alpha, s, y, Hy, sy);
     }
 
 private:
-    Hessian_updates _hessian_data;
+    Hessian_updates hessian_data;
 };
 } //end namespace detail
 
 /**
  * Performs BFGS to minimize a function f: R^n --> R with gradient g: R^m --> R^n
- * @param x
- * @param F
- * @param gradient
+ * @param x initial position
+ * @param F T valued function
+ * @param gradient of funtion
  */
 template< typename Vector, typename T>
 T BFGS( Vector& x, const std::function< T(const Vector&)>& F,
@@ -274,32 +291,32 @@ T BFGS( Vector& x, const std::function< T(const Vector&)>& F,
     auto norm_g = InfinityNorm(g);
     for( std::size_t iter=0; (norm_g > T(100)*limits::Epsilon<Base<T>>()); ++iter)
     {
-        //std::cout << "iter: " << iter << std::endl;
-        //Display(x, "Iterate");
-        //Display(g, "Gradient");
+        // std::cout << "iter: " << iter << std::endl;
+        // Display(x, "Iterate");
+        // Display(g, "Gradient");
         //Construct the quasi-newton step
         auto p = Hinv*g; p *= T(-1);
-        //Display(p," Descent direction");
-        //Evaluate the wolf conditions..
+        // Display(p," Descent direction");
+        // Evaluate the wolf conditions..
         const T stepSize = lineSearch(F, gradient, g, D, x, p);
-        //std::cout << "Step size: " << stepSize << std::endl;
-        //Update x with the step
-        //s = stepSize*p;
-        //x = x + s
+        // std::cout << "Step size: " << stepSize << std::endl;
+        // Update x with the step
+        // s = stepSize*p;
+        // x = x + s
         p *= stepSize;
         Axpy(T(1), p, x);
-        //Hold on to old gradient
+        // Hold on to old gradient
         g_old = g;
-        //Re-evaluate
+        // Re-evaluate
         gradient(x, g);
         norm_g = InfinityNorm(g);
         if( norm_g < T(100)*limits::Epsilon<Base<T>>()){ return F(x); }
-        //Evaluate change in gradient
+        // Evaluate change in gradient
         y = g;
-        //y = g - g_old
+        // y = g - g_old
         Axpy(T(-1), g_old, y);
         Hinv.update(p, y);
-        //std::cout << "||g||_inf = " << norm_g << std::endl;
+        // std::cout << "||g||_inf = " << norm_g << std::endl;
     }
     return F(x);
 }
