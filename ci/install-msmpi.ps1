b0bf4ec8f1f216fd38f076192b4a156d24760b7a
Ryan H. Lewis <me@ryanlewis.net>
2016-11-30 14:49:43 -0800

    fix scripts.

--- ci/install-msmpi.ps1
+++ ci/install-msmpi.ps1
@@ -12,38 +12,6 @@ $ScriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 . "$ScriptDir\download.ps1"
 $DOWNLOADS = "C:\Downloads\MSMPI"
 
-# Author:  Lisandro Dalcin
-# Contact: dalcinl@gmail.com
-
-function Download ($url, $filename, $destdir) {
-    if ($destdir) {
-        $item = New-Item $destdir -ItemType directory -Force
-        $destdir = $item.FullName
-    } else {
-        $destdir = $pwd.Path
-    }
-    $filepath = Join-Path $destdir $filename
-    if (Test-Path $filepath) {
-        Write-Host "Reusing" $filename "from" $destdir
-        return $filepath
-    }
-    Write-Host "Downloading" $filename "from" $url
-    $webclient = New-Object System.Net.WebClient
-    foreach($i in 1..3) {
-        try {
-            $webclient.DownloadFile($url, $filepath)
-            Write-Host "File saved at" $filepath
-            return $filepath
-        }
-        Catch [Exception] {
-            Start-Sleep 1
-        }
-    }
-    Write-Host "Failed to download" $filename "from" $url
-    return $null
-}
-
-
 function InstallMicrosoftMPISDK ($baseurl, $filename) {
     Write-Host "Installing Microsoft MPI SDK"
     $url = $baseurl + $filename
